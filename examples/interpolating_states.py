8d9119eba97bac66c55c2b1f1b65af064cea27b9
danielk <daniel.kastinen@irf.se>
2021-02-05 11:21:09 +0100

    added linear interpolation

--- examples/interpolating_states.py
+++ examples/interpolating_states.py
@@ -22,28 +22,45 @@ prop = SGP4(
 )
 
 state0 = np.array([-7100297.113,-3897715.442,18568433.707,86.771,-3407.231,2961.571])
-t = np.arange(0.0,360.0,30.0)
+t = np.arange(0.0,360.0+30.0,step=30.0)
 mjd0 = 53005
 
 states = prop.propagate(t, state0, mjd0, A=1.0, C_R = 1.0, C_D = 1.0)
 
+t_f = np.arange(0.0,360.0,step=1.0)
 
 interpolator = interpolation.Legendre8(states, t)
-
-t_f = np.arange(0.0,360.0,1.0)
 finer_states = interpolator.get_state(t_f)
+
+lin_interp = interpolation.Linear(states, t)
+lin_states = lin_interp.get_state(t_f)
+
 
 fig = plt.figure(figsize=(15,15))
 for ind in range(3):
     ax = fig.add_subplot(321 + ind)
     ax.plot(t_f, finer_states[ind,:],".r")
+    ax.plot(t_f, lin_states[ind,:],".g")
     ax.plot(t, states[ind,:],"xb")
     ax.set_title(f'R_{ind}')
 
     ax = fig.add_subplot(324 + ind)
     ax.plot(t_f, finer_states[ind+3,:],".r")
+    ax.plot(t_f, lin_states[ind+3,:],".g")
     ax.plot(t, states[ind+3,:],"xb")
     ax.set_title(f'V_{ind}')
 
+
+
+
+fig = plt.figure(figsize=(15,15))
+for ind in range(3):
+    ax = fig.add_subplot(321 + ind)
+    ax.plot(t_f, finer_states[ind,:] - lin_states[ind,:],"-b")
+    ax.set_title(f'R_{ind} diff')
+
+    ax = fig.add_subplot(324 + ind)
+    ax.plot(t_f, finer_states[ind+3,:] - lin_states[ind+3,:],"-b")
+    ax.set_title(f'V_{ind} diff')
 
 plt.show()
