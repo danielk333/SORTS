acbbcfaf1e9999a0508221c5b11b8b76a346ff4b
danielk <daniel.kastinen@irf.se>
2021-02-16 16:59:42 +0100

    fixing more table animations for terminal

--- sorts/io/terminal.py
+++ sorts/io/terminal.py
@@ -9,11 +9,12 @@ import sys
 
 from tabulate import tabulate
 
+ERASE_LINE = '\x1b[2K'
+CURSOR_UP_ONE = '\x1b[1A'
+
 def flush_table(table_data, header, chunksize = 10, step_size=10, step_time = 0.2):
     '''"Animates" a table by flushing multiple chunks of that table to the terminal with a sleep time between flushes.
     '''
-    ERASE_LINE = '\x1b[2K'
-    CURSOR_UP_ONE = '\x1b[1A'
 
     up_flush = CURSOR_UP_ONE + ERASE_LINE
 
@@ -36,3 +37,31 @@ def flush_table(table_data, header, chunksize = 10, step_size=10, step_time = 0.
             sys.stdout.write('\n '*(chunksize - len(sub_table)))
 
         sys.stdout.flush()
+
+
+def step_flush_table(table_data, header, table_size, step_time = None, first_step = False):
+    '''"Animates" a table by flushing multiple chunks of that table to the terminal with a sleep time between flushes.
+    '''
+
+    up_flush = CURSOR_UP_ONE + ERASE_LINE
+
+    lines = table_size + 3
+    if step_time is not None:
+        time.sleep(step_time)
+
+    #last possibly un-even table
+    if len(table_data) < table_size:
+        fill = '\n'*(table_size - len(table_data))
+    else:
+        fill = ''
+
+    str_data = '\n' + tabulate(table_data, headers=header) + fill + '\n'
+
+    if not first_step:
+        for j in range(lines):
+            sys.stdout.write(up_flush)
+            sys.stdout.flush()
+
+    sys.stdout.write(str_data)
+
+    sys.stdout.flush()
