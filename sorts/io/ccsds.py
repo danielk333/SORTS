28172a4f2cf954d89a76740ecf7c1cee7e4148ac
ingarantzen <ingar.arntzen@gmail.com>
2021-02-18 18:46:57 +0100

    finished OEM and TDM messages

--- sorts/io/ccsds.py
+++ sorts/io/ccsds.py
@@ -2,27 +2,34 @@
 
 '''CCSDS TDM/OEM file input outputs
 
-https://public.ccsds.org/Pubs/503x0b1c1.pdf
+Orbit Data Messages  
+ODM     502.0-B-2   Blue Book (11/2009)  
+https://public.ccsds.org/Pubs/502x0b2c1e2.pdf
+
+Tracking Data Message
+TDM     503.0-B-2   Blue Book (06/2020) 
+https://public.ccsds.org/Pubs/503x0b2.pdf
+
+NPM (Navigation Data Messages)
 https://public.ccsds.org/Pubs/505x0b1.pdf
 
+NPM Schemas covering TDM and OEM and more
+https://sanaregistry.org/files/ndmxml_unqualified/*
+https://sanaregistry.org/files/ndmxml_qualified/*
+
+
 '''
+
 import pkg_resources
-import pathlib
-import time
-import datetime
+# import time
+# import datetime
 import os
-
-from lxml import etree
 import xmlschema
-
-import numpy as np
-import scipy.constants as consts
+# import numpy as np
+# import scipy.constants as consts
 from astropy.time import TimeISO, Time
-
-
-from .. import dates
+# from .. import dates
 from ..version import __version__
-
 
 
 class epochType(TimeISO):
@@ -37,23 +44,6 @@ class epochType(TimeISO):
     )
 
 
-def write_txt_tdm():
-    raise NotImplementedError()
-def read_txt_tdm():
-    raise NotImplementedError()
-def read_xml_tdm():
-    raise NotImplementedError()
-def write_txt_oem():
-    raise NotImplementedError()
-def read_txt_oem():
-    raise NotImplementedError()
-def write_xml_oem():
-    raise NotImplementedError()
-def read_xml_oem():
-    raise NotImplementedError()
-
-
-
 # needs only to be unique - does not matter what it points to
 # blue book examples for both tdm versions 1 and version 2 have this,
 # (even though it points to what appears to be a version 1 specific resource)
@@ -62,18 +52,205 @@ def read_xml_oem():
 SCHEMA_URI = 'http://sanaregistry.org/r/ndmxml/ndmxml-1.0-master.xsd'
 
 
-_TDM_SCHEMA = None
 
-def get_tdm_schema():
-    global _TDM_SCHEMA
-    if _TDM_SCHEMA is None:
+###############################################################################
+# NDM SCHEMA
+###############################################################################
+
+_SCHEMA = None
+
+def get_schema():
+    global _SCHEMA
+    if _SCHEMA is None:
         data_path = pkg_resources.resource_filename('sorts', 'data')
         xsd_path = os.path.join(data_path, 'ndmxml-2.0.0-master-2.0.xsd')
-        _TDM_SCHEMA = xmlschema.XMLSchema(xsd_path)
-    return _TDM_SCHEMA
+        _SCHEMA = xmlschema.XMLSchema(xsd_path)
+    return _SCHEMA
 
 
-TDM_OBSERVATION_METADATA_FIELDS = [
+###############################################################################
+# OEM
+###############################################################################
+
+
+_OEM_METADATA_FIELDS = [
+    "COMMENT",
+    "OBJECT_NAME",
+    "OBJECT_ID",
+    "CENTER_NAME",
+    "REF_FRAME",
+    "REF_FRAME_EPOCH",
+    "TIME_SYSTEM",
+    "START_TIME",
+    "USEABLE_START_TIME",
+    "USEABLE_STOP_TIME",
+    "STOP_TIME",
+    "INTERPOLATION",
+    "INTERPOLATION_DEGREE"
+]
+
+###############################################################################
+# OEM READ
+###############################################################################
+
+def read_xml_oem(xml):
+    """
+    read xml OEM file resource, parse and validate according to schema,
+    returns python dictionary representing document
+    """
+    schema = get_schema()
+    d = schema.decode(xml)
+    return d
+
+
+###############################################################################
+# OEM WRITE
+###############################################################################
+
+def write_xml_oem(data, meta, file=None):    
+    """
+    creaate and validate xml for OEM message
+    write xml file resource
+    """
+    # originator
+    originator = meta.get("ORIGINATOR", f'SORTS {__version__}')
+
+    # creation data
+    creation_date = meta.get("CREATION_DATE", Time.now()).CCSDS_epoch
+
+    # tdm dictionary
+    d = {
+        '@id': 'CCSDS_OEM_VERS',
+        '@version': '2.0',
+        '@xmlns:xsi': 'http://www.w3.org/2001/XMLSchema-instance',
+        '@xsi:noNamespaceSchemaLocation': SCHEMA_URI,
+        'header': {
+            'COMMENT': "HEADER COMMENT",
+            'CREATION_DATE': f'{creation_date}', 
+            'ORIGINATOR': f'{originator}'
+        },
+        'body': {
+            'segment': []
+        }
+    }
+
+    # add segments
+    d['body']['segment']
+
+    # ALTERNATIVES
+    # 
+    # 1) - make one segment per observation or
+    # 2) - one segment with multiple observations
+    #
+    # CHOOSE 2)
+    segment = {
+        "metadata": {},
+        "data": {}
+    }
+    d['body']['segment'].append(segment)    
+
+    ###########################################################################
+    # SEGMENT METADATA
+    ###########################################################################
+
+    _meta = segment["metadata"]
+
+    # mandatory
+    meta_defaults = dict(
+        OBJECT_NAME = "missing",
+        OBJECT_ID = "missing",
+        CENTER_NAME = "missing",
+        REF_FRAME = "missing",
+        TIME_SYSTEM = "UTC",
+        START_TIME = Time.now().CCSDS_epoch,
+        STOP_TIME = Time.now().CCSDS_epoch,
+    )
+    
+    # add values from given meta - in correct order
+    for field in _OEM_METADATA_FIELDS:
+        if field in meta:
+            _meta[field] = meta[field]
+        else:
+            if field in meta_defaults:
+                _meta[field] = meta_defaults[field]
+
+    ###########################################################################
+    # SEGMENT DATA
+    ###########################################################################
+
+    _data = segment["data"]
+    _data["COMMENT"] = 'DATA COMMENT'
+    _data["stateVector"] = []
+
+    mock_vectors = [
+        [Time.now().CCSDS_epoch, 1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9],
+        [Time.now().CCSDS_epoch, 1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9],
+    ]
+
+    for v in mock_vectors:
+        _data["stateVector"].append(dict(
+            EPOCH = v[0],
+            X = v[1],
+            Y = v[2],
+            Z = v[3],
+            X_DOT = v[4],
+            Y_DOT = v[5],
+            Z_DOT = v[6],
+            X_DDOT = v[7],
+            Y_DDOT = v[8],
+            Z_DDOT = v[9]
+        ))
+
+
+    mock_matrices = [
+        [1.1]*21,
+        [2.2]*21
+    ]
+
+    _data["covarianceMatrix"] = []
+    for m in mock_matrices:
+        _data["covarianceMatrix"].append(dict(
+            COMMENT = "COMMENT",
+            EPOCH = Time.now().CCSDS_epoch,
+            COV_REF_FRAME= "missing",
+            CX_X = m[0],
+            CY_X = m[1],
+            CY_Y = m[2],
+            CZ_X = m[3],
+            CZ_Y = m[4],
+            CZ_Z = m[5],
+            CX_DOT_X = m[6],
+            CX_DOT_Y = m[7],
+            CX_DOT_Z = m[8],
+            CX_DOT_X_DOT = m[9],
+            CY_DOT_X = m[10],
+            CY_DOT_Y = m[11],
+            CY_DOT_Z = m[12],
+            CY_DOT_X_DOT = m[13],
+            CY_DOT_Y_DOT = m[14],
+            CZ_DOT_X = m[15],
+            CZ_DOT_Y = m[16],
+            CZ_DOT_Z = m[17],
+            CZ_DOT_X_DOT = m[18],
+            CZ_DOT_Y_DOT = m[19],
+            CZ_DOT_Z_DOT = m[20]
+        ))
+    
+    # serialize to xml
+    schema = get_schema()
+    xml_etree = schema.encode(d, path='/oem')
+    
+    if file is not None:
+        file.write(xmlschema.etree_tostring(xml_etree, encoding='unicode', method='xml'))
+
+    return xml_etree
+
+
+###############################################################################
+# TDM
+###############################################################################
+
+_TDM_METADATA_FIELDS = [
     "COMMENT",
     "TRACK_ID",
     "TRACK_ID",
@@ -136,13 +313,38 @@ TDM_OBSERVATION_METADATA_FIELDS = [
 ]
 
 
-def write_xml_tdm(data, meta, file=None):
+###############################################################################
+# TDM READ
+###############################################################################
 
+
+def read_xml_tdm(xml):
+    """
+    read xml TMD file resource, parse and validate according to schema,
+    returns python dictionary representing document
+    """
+    schema = get_schema()
+    d = schema.decode(xml)
+    return d
+
+
+###############################################################################
+# TDM WRITE
+###############################################################################
+
+def write_xml_tdm(data, meta, file=None):
+    """
+    creaate and validate xml for TDM message
+    write xml file resource
+    """
     # originator
     originator = meta.get("ORIGINATOR", f'SORTS {__version__}')
 
     # creation data
     creation_date = meta.get("CREATION_DATE", Time.now()).CCSDS_epoch
+
+    # message id
+    message_id = meta.get("MESSAGE_ID", 0)
 
     # tdm dictionary
     d = {
@@ -152,7 +354,8 @@ def write_xml_tdm(data, meta, file=None):
         '@xsi:noNamespaceSchemaLocation': SCHEMA_URI,
         'header': {
             'CREATION_DATE': f'{creation_date}', 
-            'ORIGINATOR': f'{originator}'
+            'ORIGINATOR': f'{originator}',
+            'MESSAGE_ID': f'{message_id}'
         },
         'body': {
             'segment': []
@@ -187,7 +390,7 @@ def write_xml_tdm(data, meta, file=None):
     }
     
     # add values from given meta - in correct order
-    for field in TDM_OBSERVATION_METADATA_FIELDS:
+    for field in _TDM_METADATA_FIELDS:
         if field in meta:
             _meta[field] = meta[field]
         else:
@@ -209,7 +412,7 @@ def write_xml_tdm(data, meta, file=None):
         })
 
     # serialize to xml
-    schema = get_tdm_schema()
+    schema = get_schema()
     xml_etree = schema.encode(d, path='/tdm')
     
     if file is not None:
@@ -217,245 +420,3 @@ def write_xml_tdm(data, meta, file=None):
 
     return xml_etree
 
-
-
-
-
-
-
-# def write_oem(t, state, meta, fname=None):
-#     '''Uses a series of astropy times and state vectors to create a CCSDS OEM file (plain-text of xml) or return a string.
-
-#     :param astropy.Time t: Vector of unix-times
-#     :param numpy.ndarray state: 6-D states given in SI units in the ITRF2000 frame. Rows correspond to different states and columns to dimensions.
-#     :param dict meta: Dict containing all the standard CCSDS 2.0 meta data.
-#     :param bool xml: If `False`, use the plain-text format
-#     :param str fname: Output file-path for OEM.
-#     '''
-#     if xml:
-#         raise NotImplementedError('')
-    
-#     fo.write("CCSDS_OEM_VERS = 2.0\n")
-#     fo.write("CREATION_DATE = %s\n"%(dates.unix_to_datestr(t.min()))) # 1996-11-04T17:22:31
-#     fo.write("ORIGINATOR = SORTS\n")
-#     fo.write("META_START\n")
-#     fo.write("OBJECT_NAME = %s\n"%(oid))
-#     fo.write("OBJECT_ID = %d\n"%(oid))
-#     fo.write("CENTER_NAME = EARTH\n")
-#     fo.write("REF_FRAME = ITRF2000\n")
-#     fo.write("TIME_SYSTEM = UTC\n")
-#     fo.write("START_TIME = %s\n"%(dates.unix_to_datestrf(t.min())))
-#     fo.write("USEABLE_START_TIME = %s\n"%(dates.unix_to_datestrf(t.min())))
-#     fo.write("USEABLE_STOP_TIME = %s\n"%(dates.unix_to_datestrf(t.max())))
-#     fo.write("STOP_TIME = %s\n"%(dates.unix_to_datestrf(t.max())))
-#     fo.write("META_STOP\n")
-#     fo.write("COMMENT This file was produced by SORTS.\n")
-#     fo.write("COMMENT.\n")
-#     for ti in range(len(t)):
-#         fo.write("%s %1.3f %1.3f %1.3f %1.3f %1.3f %1.3f\n"%(dates.unix_to_datestrf(t[ti]),state[ti,0],state[ti,1],state[ti,2],state[ti,3],state[ti,4],state[ti,5]))
-    
-#     if fname is not None:
-
-#         fo=open(fname,"w")
-#         fo.close()
-
-
-# def read_oem(fname, xml=True):
-#     '''Todo: docstring
-#     '''
-#     if xml:
-#         parser = etree.XMLParser(schema = _get_oem_schema())
-#         tree = etree.parse(open(fname, 'r'), parser)
-#         return tree
-
-#     else:
-#         meta = {'COMMENT': ''}
-
-#         _dtype = [
-#             ('date', 'datetime64[us]'),
-#             ('x', 'float64'),
-#             ('y', 'float64'),
-#             ('z', 'float64'),
-#             ('vx', 'float64'),
-#             ('vy', 'float64'),
-#             ('vz', 'float64'),
-#         ]
-
-#         raw_data = []
-#         DATA_ON = False
-#         META_ON = False
-#         with open(fname, 'r') as f:
-#             for line in f:
-
-#                 if META_ON:
-#                     if line.strip() == 'META_STOP':
-#                         META_ON = False
-#                         DATA_ON = True
-#                         continue
-
-#                     _tmp = [x.strip() for x in line.split('=')]
-#                     meta[_tmp[0]] = _tmp[1]
-#                 elif DATA_ON:
-#                     if line[:7] == 'COMMENT':
-#                         meta['COMMENT'] += line[7:]
-#                     else:
-#                         raw_data.append(line.split(' '))
-#                 else:
-#                     if line.strip() == 'META_START':
-#                         META_ON = True
-#                         continue
-#                     _tmp = [x.strip() for x in line.split('=')]
-#                     meta[_tmp[0]] = _tmp[1]
-
-
-#         data_len = len(raw_data)
-
-#         data = np.empty((data_len, ), dtype=_dtype)
-
-#         for ind, row in enumerate(raw_data):
-#             rown = 0
-#             for col, dtype in _dtype:
-#                 data[ind][col] = row[rown]
-#                 rown += 1
-
-#         return data, meta
-
-
-
-# def write_tdm(
-#         datas,
-#         meta,
-#         fname=None,
-#     ):
-#     '''
-#     # TODO: Document function.
-#     # TODO: Update function to be more general
-#     # TODO: add arbitrary fields and meta data
-#     '''
-#     segments = xml_tdm_segmet(datas)
-#     root = xml_tdm_tree(meta, segments)
-
-#     fo=open(fname,"w")
-#     fo.write("CCSDS_TDM_VERS = 1.0\n")
-#     fo.write("   COMMENT MASTER ID %s\n"%(oid))
-#     fo.write("   COMMENT TX_ECEF (%1.12f,%1.12f,%1.12f)\n"%(tx_ecef[0],tx_ecef[1],tx_ecef[2]))
-#     fo.write("   COMMENT RX_ECEF (%1.12f,%1.12f,%1.12f)\n"%(rx_ecef[0],rx_ecef[1],rx_ecef[2]))
-#     fo.write("   COMMENT This is a simulated %s of MASTER ID %s with EISCAT 3D\n"%(tdm_type,oid))
-#     fo.write("   COMMENT 233 MHz, time of flight, with ionospheric corrections\n")
-#     fo.write("   COMMENT EISCAT 3D coordinates: \n")
-#     fo.write("   COMMENT Author(s): SORTS.\n")
-#     fo.write("   CREATION_DATE       = %s\n"%(dates.unix_to_datestr(time.time())))
-#     fo.write("META_START\n")
-#     fo.write("   TIME_SYSTEM         = UTC\n")
-#     fo.write("   START_TIME          = %s\n"%(dates.unix_to_datestrf(t_pulse.min())))
-#     fo.write("   STOP_TIME           = %s\n"%(dates.unix_to_datestrf(t_pulse.max())))
-#     fo.write("   PARTICIPANT_1       = %s\n"%(tx_name))
-#     fo.write("   PARTICIPANT_2       = %s\n"%(oid))
-#     fo.write("   PARTICIPANT_3       = %s\n"%(rx_name))
-#     fo.write("   MODE                = SEQUENTIAL\n")
-#     fo.write("   PATH                = 1,2,3\n")
-#     fo.write("   TRANSMIT_BAND       = %1.5f\n"%(freq))
-#     fo.write("   RECEIVE_BAND        = %1.5f\n"%(freq))
-#     fo.write("   TIMETAG_REF         = TRANSMIT\n")
-#     fo.write("   INTEGRATION_REF     = START\n")
-#     fo.write("   RANGE_MODE          = CONSTANT\n")
-#     fo.write("   RANGE_MODULUS       = %1.2f\n"%(128.0*20e-3))
-#     fo.write("   RANGE_UNITS         = KM\n")
-#     fo.write("   DATA_QUALITY        = VALIDATED\n")
-#     fo.write("   CORRECTION_RANGE    = 0.0\n")
-#     fo.write("   NOISE_DATA          = ON\n")
-#     fo.write("   CORRECTIONS_APPLIED = NO\n")
-#     fo.write("META_STOP\n")
-#     fo.write("DATA_START\n")
-#     for ri in range(len(t_pulse)):
-#         fo.write("   RANGE                   = %s %1.12f %1.12f\n"%(dates.unix_to_datestrf(t_pulse[ri]),m_range[ri], m_range_std[ri]))
-#         fo.write("   DOPPLER_INSTANTANEOUS   = %s %1.12f %1.12f\n"%(dates.unix_to_datestrf(t_pulse[ri]),m_range_rate[ri], m_range_rate_std[ri]))
-#     fo.write("DATA_STOP\n")
-#     fo.close()
-
-
-# def read_tdm(fname, xml=True):
-#     '''Just get the range data # TODO: the rest
-
-#     # TODO: Document function.
-#     # TODO: Update plain text function to be more general and conform to CCSDS 2.0
-#     '''
-
-#     if xml:
-#         parser = etree.XMLParser(schema = _get_tdm_schema())
-#         tree = etree.parse(open(fname, 'r'), parser)
-#         return tree
-
-#     else:
-
-#         meta = {'COMMENT': ''}
-
-#         RANGE_UNITS = 'km'
-#         with open(fname, 'r') as f:
-#             DATA_ON = False
-#             META_ON = False
-#             data_raw = {}
-#             for line in f:
-#                 if line.strip() == 'DATA_STOP':
-#                     break
-
-#                 if META_ON:
-#                     tmp_lin = line.split('=')
-#                     if len(tmp_lin) > 1:
-#                         meta[tmp_lin[0].strip()] = tmp_lin[1].strip()
-#                         if tmp_lin[0].strip() == 'RANGE_UNITS':
-#                             RANGE_UNITS = tmp_lin[1].strip().lower()
-#                 elif DATA_ON:
-#                     name, tmp_dat = line.split('=')
-
-#                     name = name.strip().lower()
-#                     tmp_dat = tmp_dat.strip().split(' ')
-
-#                     if name in data_raw:
-#                         data_raw[name].append(tmp_dat)
-#                     else:
-#                         data_raw[name] = [tmp_dat]
-#                 else:
-#                     if line.lstrip()[:7] == 'COMMENT':
-#                         meta['COMMENT'] += line.lstrip()[7:]
-#                     else:
-#                         tmp_lin = line.split('=')
-#                         if len(tmp_lin) > 1:
-#                             meta[tmp_lin[0].strip()] = tmp_lin[1].strip()
-
-#                 if line.strip() == 'META_START':
-#                     META_ON = True
-#                 if line.strip() == 'DATA_START':
-#                     META_ON = False
-#                     DATA_ON = True
-#         _dtype = [
-#             ('date', 'datetime64[us]'),
-#         ]
-
-#         data_len = len(data_raw[data_raw.keys()[0]])
-
-#         for name in data_raw:
-#             _dtype.append( (name, 'float64') )
-#             _dtype.append( (name + '_err', 'float64') )
-
-#         data = np.empty((data_len, ), dtype=_dtype)
-
-#         date_set = False
-#         for name, series in data_raw.items():
-#             for ind, val in enumerate(series):
-#                 if not date_set:
-#                     data[ind]['date'] = np.datetime64(val[0],'us')
-
-#                 data[ind][name] = np.float64(val[1])
-#                 if len(val) > 2:
-#                     data[ind][name + '_err'] = np.float64(val[2])
-#                 else:
-#                     data[ind][name + '_err'] = 0.0
-
-#                 if name == 'range':
-#                     if RANGE_UNITS == 's':
-#                         data[ind][name] *= consts.c*1e-3
-
-#             date_set = True
-
-#         return data, meta
