09bb1159df1921f0f96f18493c796fd456aebdd6
danielk <daniel.kastinen@irf.se>
2021-05-20 22:22:58 +0200

    added example of using the ionospheric error estimation

--- examples/adding_random_errors.py
+++ examples/adding_random_errors.py
@@ -39,4 +39,33 @@ axes[0,1].hist(10*np.log10(snrs), 100)
 axes[1,0].plot(range(num), perturbed_ranges, 100)
 axes[1,1].hist(ranges - perturbed_ranges, 100)
 
-plt.show()
+
+print(f'Caching error calculation data to: {pth}')
+
+
+eiscat3d.tx[0].bandwidth = 10000.0
+err = errors.LinearizedCodedIonospheric(eiscat3d.tx[0], seed=123, cache_folder=pth)
+
+num2 = 400
+
+ranges = [300e3, 1000e3, 2000e3]
+
+range_rates = np.linspace(0, 10e3, num=num2)
+snrs_db = np.linspace(14,50,num=num2)
+snrs = 10.0**(snrs_db*0.1)
+
+v_std = err.range_rate_std(snrs)
+
+fig, axes = plt.subplots(1,2)
+
+for j in range(len(ranges)):
+    range_ = np.zeros_like(snrs)
+    range_[:] = ranges[j]
+    r_std = err.range_std(range_, snrs)
+    axes[0].plot(snrs_db, r_std, label=f'Range: {ranges[j]*1e-3:.1f} km')
+
+axes[0].legend()
+axes[1].plot(snrs_db, v_std)
+
+
+plt.draw()
