16594bd55485a5f05d23d8c530e7ff2f30beddb1
danielk <daniel.kastinen@irf.se>
2022-03-10 14:39:56 +0100

    orbital plotting, useful helper function

--- sorts/plotting/__init__.py
+++ sorts/plotting/__init__.py
@@ -4,7 +4,8 @@
 
 '''
 
-from .general import orbits, grid_earth, set_axes_equal
+from .general import grid_earth, set_axes_equal
 from .radar import radar_earth, radar_map
 from .tracking import local_tracking, local_passes
 from .schedule import schedule_pointing, observed_parameters
+from .orbits import kepler_scatter, kepler_orbit
