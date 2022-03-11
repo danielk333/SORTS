85a182cb6811d94212a6b1de80a69bc84138a9aa
danielk <daniel.kastinen@irf.se>
2022-03-11 10:37:15 +0100

    expose in top level

--- sorts/io/__init__.py
+++ sorts/io/__init__.py
@@ -6,4 +6,5 @@
 
 from .ccsds import write_xml_tdm, read_xml_tdm
 from .ccsds import write_xml_oem, read_xml_oem
+from .ccsds import read_txt_oem, read_txt_tdm
 from .terminal import flush_table, step_flush_table
