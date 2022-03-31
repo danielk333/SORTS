aecacb1fd6074a14b1b8cefcf293ce8e36a3cb91
danielk <daniel.kastinen@irf.se>
2022-03-31 08:46:42 +0200

    pep8 and allow kw-args to fov function

--- sorts/radar/radar.py
+++ sorts/radar/radar.py
@@ -4,21 +4,29 @@
 
 '''
 import copy
-import numpy as np
 
 from .. import passes
+
 
 class Radar(object):
     '''A network of transmitting and receiving radar stations.
         
-        :ivar list tx: List of transmitting sites, i.e. instances of :class:`sorts.radar.TX`
-        :ivar list rx: List of receiving sites, i.e. instances of :class:`sorts.radar.RX`
-        :ivar float min_SNRdb: Minimum SNR detectable by radar system in dB (after coherent integration).
-        :ivar list joint_stations: A list of (tx,rx) indecies of stations that share hardware. This can be used to e.g. turn of receivers when the same hardware is transmitting.
+        :ivar list tx: List of transmitting sites, 
+            i.e. instances of :class:`sorts.radar.TX`
+        :ivar list rx: List of receiving sites, 
+            i.e. instances of :class:`sorts.radar.RX`
+        :ivar float min_SNRdb: Minimum SNR detectable by radar system in dB 
+            (after coherent integration).
+        :ivar list joint_stations: A list of (tx,rx) indecies of stations that 
+            share hardware. This can be used to e.g. turn of receivers when 
+            the same hardware is transmitting.
 
-        :param list tx: List of transmitting sites, i.e. instances of :class:`sorts.radar.TX`
-        :param list rx: List of receiving sites, i.e. instances of :class:`sorts.radar.RX`
-        :param float min_SNRdb: Minimum SNR detectable by radar system in dB (after coherent integration).
+        :param list tx: List of transmitting sites, 
+            i.e. instances of :class:`sorts.radar.TX`
+        :param list rx: List of receiving sites, 
+            i.e. instances of :class:`sorts.radar.RX`
+        :param float min_SNRdb: Minimum SNR detectable by radar system in dB 
+            (after coherent integration).
 
     '''
     def __init__(self, tx, rx, min_SNRdb=10.0, joint_stations=None):
@@ -29,7 +37,6 @@ class Radar(object):
             self.joint_stations = []
         else:
             self.joint_stations = joint_stations
-
 
     def copy(self):
         '''Create a deep copy of the radar system.
@@ -46,7 +53,6 @@ class Radar(object):
             ret.rx.append(rx.copy())
         return ret
 
-
     def set_beam(self, beam):
         '''Sets the radiation pattern for transmitters and receivers.
         
@@ -54,7 +60,6 @@ class Radar(object):
         '''
         self.set_tx_beam(beam)
         self.set_rx_beam(beam)
-
 
     def set_tx_beam(self, beam):
         '''Sets the radiation pattern for transmitters.
@@ -72,20 +77,27 @@ class Radar(object):
         for rx in self.rx:
             rx.beam = beam.copy()
 
+    def find_passes(self, t, states, cache_data=True, fov_kw=None):
+        '''Finds all passes that are simultaneously inside a transmitter 
+        station FOV and a receiver station FOV. 
 
-    def find_passes(self, t, states, cache_data=True):
-        '''Finds all passes that are simultaneously inside a transmitter station FOV and a receiver station FOV. 
-
-            :param numpy.ndarray t: Vector of times in seconds to use as a base to find passes.
-            :param numpy.ndarray states: ECEF states of the object to find passes for.
-            :return: list of passes indexed by first tx-station and then rx-station.
+            :param numpy.ndarray t: Vector of times in seconds to use as a 
+                base to find passes.
+            :param numpy.ndarray states: ECEF states of the object to find 
+                passes for.
+            :return: list of passes indexed by first tx-station and then 
+                rx-station.
             :rtype: list of list of sorts.Pass
         '''
         rd_ps = []
-        for txi,tx in enumerate(self.tx):
+        for txi, tx in enumerate(self.tx):
             rd_ps.append([])
-            for rxi,rx in enumerate(self.rx):
-                txrx = passes.find_simultaneous_passes(t, states, [tx, rx], cache_data=cache_data)
+            for rxi, rx in enumerate(self.rx):
+                txrx = passes.find_simultaneous_passes(
+                    t, states, 
+                    [tx, rx], 
+                    cache_data=cache_data, fov_kw=fov_kw,
+                )
                 for ps in txrx:
                     ps.station_id = [txi, rxi]
                 rd_ps[-1].append(txrx)
