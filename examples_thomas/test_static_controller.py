863f2454021f3512e0ca2ae5d500c5b237228b45
Thomas <maynadie.t@gmail.com>
2022-05-22 16:01:14 +0200

    updated examples_thomas : added testing for the validation of the architecture of the static controller

--- examples_thomas/test_static_controller.py
+++ examples_thomas/test_static_controller.py
@@ -32,29 +32,52 @@ logger = profiling.get_logger('scanning')
 p.start("Total")
 static_controller = controllers.Static(profiler=p, logger=logger)
 
-t_slice = 0.1
-t = np.arange(0, end_t, t_slice)
-controls = static_controller.generate_controls(t, eiscat3d, t_slice=t_slice, max_points=100)
-
-# plot the generated controls
-p.start("examples_main:retreiving_control_values")
-
-plt_ids = np.linspace(0, int(end_t/t_slice), nbplots, dtype=int)
-
-print(plt_ids)
-
-for i in range(len(controls["beam_orientation"])):
-    ctrl = next(controls["beam_orientation"][i])
-    print(f"control sub-array {i} size : {(ctrl['tx'].itemsize*np.size(ctrl['tx']) + ctrl['rx'].itemsize*np.size(ctrl['rx']))/1e6} Mb")
-    
-    if i in plt_ids:
-        plotting.plot_beam_directions(ctrl, eiscat3d, logger=logger, profiler=p, zoom_level=0.95)
-
-    del ctrl
-    
-p.stop("examples_main:retreiving_control_values")
+print("Scanner init")
+dwell = 0.1
+t = np.arange(0, end_t, dwell)
+controls = static_controller.generate_controls(t, eiscat3d, dwell=dwell)
+print("Controls generated")
 
 p.stop("Total")
-print(p)
+
+fig = plt.figure(figsize=(15,15))
+ax = fig.add_subplot(111, projection='3d')
+   
+plotting.grid_earth(ax)
+
+print("Plotting")
+
+for tx in eiscat3d.tx:
+    ax.plot([tx.ecef[0]],[tx.ecef[1]],[tx.ecef[2]],'or')
+for rx in eiscat3d.rx:
+    ax.plot([rx.ecef[0]],[rx.ecef[1]],[rx.ecef[2]],'og')
+    
+for txi, tx in enumerate(eiscat3d.tx):
+    points_tx = controls["beam_direction_tx"][txi]
+
+    for rxi, rx in enumerate(eiscat3d.rx):
+        points_rx = controls["beam_direction_rx"][rxi][txi]
+        
+        for ir in range(len(points_rx)):
+            for j in range(len(points_rx[ir, 0])):
+                ktx = points_tx[:, j]
+                krx = points_rx[ir, :, j]
+            
+                a=np.dot(ktx, krx)
+                
+                if abs(abs(a)-1) < 0.0001:
+                    ecef = points_rx[ir, :, j]
+                else:
+                    ecef = tx.ecef + np.dot(tx.ecef - rx.ecef, a*krx - ktx)/(1 - a**2)*ktx
+                
+                    ax.plot([tx.ecef[0], ecef[0]], [tx.ecef[1], ecef[1]], [tx.ecef[2], ecef[2]], 'r-', alpha=0.15)
+                    ax.plot([rx.ecef[0], ecef[0]], [rx.ecef[1], ecef[1]], [rx.ecef[2], ecef[2]], 'g-', alpha=0.15)
+
+dr = 600e3
+ax.set_xlim([eiscat3d.tx[0].ecef[0]-dr, eiscat3d.tx[0].ecef[0]+dr])
+ax.set_ylim([eiscat3d.tx[0].ecef[1]-dr, eiscat3d.tx[0].ecef[1]+dr])
+ax.set_zlim([eiscat3d.tx[0].ecef[2]-dr, eiscat3d.tx[0].ecef[2]+dr])
 
 plt.show()
+
+print(p)
