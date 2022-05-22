e05012646b899d1939fab28d4a4f691bf3ebad54
Thomas <maynadie.t@gmail.com>
2022-05-22 10:55:39 +0200

    updated fence.py : vectorized computations

--- sorts/radar/scans/fence.py
+++ sorts/radar/scans/fence.py
@@ -47,13 +47,10 @@ class Fence(Scan):
 
     def pointing(self, t):
         ind = (np.mod(t/self.cycle(), 1)*self.num).astype(np.int)
-        if isinstance(t, float) or isinstance(t, int):
-            shape = (3, )
-        else:
-            shape = (3, len(t))
-
-        azelr = np.empty(shape, dtype=np.float64)
+        
+        azelr = np.empty((3, np.size(t)), dtype=np.float64)
         azelr[0,...] = self._az[ind]
         azelr[1,...] = self._el[ind]
         azelr[2,...] = 1.0
+        
         return azelr
