6fd0c2e4e65aa1269733d34d142891bb796e2b97
Thomas <maynadie.t@gmail.com>
2022-05-24 08:08:07 +0200

    Added plotting of beam orientation controls

--- sorts/plotting/__init__.py
+++ sorts/plotting/__init__.py
@@ -9,6 +9,7 @@ from .radar import radar_earth, radar_map
 from .tracking import local_tracking, local_passes
 from .schedule import schedule_pointing, observed_parameters
 from .scan import scan, plot_radar_scan_movie
-
+from .controls import plot_beam_directions
 from .orbits import kepler_scatter, kepler_orbit
+
 from . import colors
