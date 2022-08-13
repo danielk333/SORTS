52215ac816c60c2be6e652199ec683dca03d3a41
danielk <daniel.kastinen@irf.se>
2022-08-13 20:17:21 +0200

    updating setup configuration

--- setup.py
+++ setup.py
@@ -1,24 +1,31 @@
 import setuptools
-import pathlib
-import codecs
-import sys
+from distutils.core import Extension
 
-HERE = pathlib.Path(__file__).resolve().parents[0]
-
-def get_version(path):
-    with codecs.open(path, 'r') as fp:
-        for line in fp.read().splitlines():
-            if line.startswith('__version__'):
-                delim = '"' if '"' in line else "'"
-                return line.split(delim)[1]
-        else:
-            raise RuntimeError("Unable to find version string.")
+clibsorts = Extension(
+    name='sorts.clibsorts',
+    sources=[
+        'src/clibsorts/radar_controller.c',
+        # 'src/clibsorts/static_priority_scheduler.c',
+        # 'src/clibsorts/plotting_controls.c',
+        # 'src/clibsorts/signals.c',
+        # 'src/clibsorts/measurements.c',
+        # 'src/clibsorts/radar.c',
+    ],
+    include_dirs=[
+        'src/clibsorts/',
+    ],
+    extra_compile_args=['-std=c99', '-fPIC'],
+)
 
 setuptools.setup(
-    version=get_version(HERE / 'sorts' / 'version.py'),
-    packages=setuptools.find_packages(),
-    extensions = ['sphinx.ext.pngmath', 'sphinx.ext.todo', 'breathe'],
-    package_data={
+    package_dir = {
+        "": "src"
+    },
+    packages=setuptools.find_packages(where="src"),
+    ext_modules = [
+        clibsorts,
+    ],
+    package_data = {
         'sorts': ['data/*'],
     },
 )
