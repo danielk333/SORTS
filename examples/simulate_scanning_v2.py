29553a721708b02ad080d7911ef80072b777d073
tszhinh <tsz-hin.heung@irf.se>
2025-04-15 13:30:16 +0200

    - adj import - commented out a portion of code - save data using pickle

--- examples/simulate_scanning_v2.py
+++ examples/simulate_scanning_v2.py
@@ -4,11 +4,10 @@
 Simulate scanning for objects
 ======================================
 """
-
+import os, pickle
+from pathlib import Path
 import numpy as np
 import matplotlib.pyplot as plt
-
-
 import sorts
 from sorts.scheduler import StaticList, ObservedParameters
 from sorts.controller import Scanner
@@ -17,7 +16,7 @@ from sorts.radar.scans import Fence
 
 from sorts.propagator import SGP4
 
-eiscat3d = sorts.radars.eiscat3d
+eiscat3d = sorts.get_radar("eiscat3d", "stage1-array")
 
 Prop_cls = SGP4
 Prop_opts = dict(
@@ -56,7 +55,9 @@ class ObservedScanning(StaticList, ObservedParameters):
 
 
 scanner_ctrl = Scanner(
-    eiscat3d, scan, t=np.arange(0, end_t, scan.dwell()),
+    eiscat3d,
+    scan,
+    t=np.arange(0, end_t, scan.dwell()),
 )
 
 scheduler = ObservedScanning(
@@ -64,117 +65,129 @@ scheduler = ObservedScanning(
     controllers=[scanner_ctrl],
 )
 
-
+# load saved data if there is a save, otherwise generate and save them
 datas = []
 passes = []
 states = []
-for ind in range(len(objs)):
-    t = sorts.equidistant_sampling(
-        orbit=objs[ind].state,
-        start_t=0,
-        end_t=end_t,
-        max_dpos=1e3,
-    )
-
-    print(f"Temporal points obj {ind}: {len(t)}")
-
-    states += [objs[ind].get_state(t)]
-
-    # rename cache_data to something more descriptive
-    passes += [eiscat3d.find_passes(t, states[ind], cache_data=True)]
-
-    data = scheduler.observe_passes(passes[ind], space_object=objs[ind], snr_limit=False)
-
-    datas.append(data)
-
-
-fig = plt.figure(figsize=(15, 15))
-axes = [
-    [
-        fig.add_subplot(221, projection="3d"),
-        fig.add_subplot(222),
-    ],
-    [
-        fig.add_subplot(223),
-        fig.add_subplot(224),
-    ],
-]
-
-sorts.plotting.grid_earth(axes[0][0])
-for tx in eiscat3d.tx:
-    axes[0][0].plot([tx.ecef[0]], [tx.ecef[1]], [tx.ecef[2]], "or")
-for rx in eiscat3d.rx:
-    axes[0][0].plot([rx.ecef[0]], [rx.ecef[1]], [rx.ecef[2]], "og")
-
-for radar, meta in scanner_ctrl(np.arange(0, scan.cycle(), scan.dwell())):
-    for tx in radar.tx:
-        point_tx = (
-            tx.pointing_ecef / np.linalg.norm(tx.pointing_ecef, axis=0) * scanner_ctrl.r.max()
-            + tx.ecef
-        )
-        axes[0][0].plot(
-            [tx.ecef[0], point_tx[0]],
-            [tx.ecef[1], point_tx[1]],
-            [tx.ecef[2], point_tx[2]],
-            "r-",
-            alpha=0.15,
+pickle_fpath = (
+    Path(os.path.dirname(os.path.abspath(__file__))) / "simulate_scanning_v2__saves.pickle"
+)
+if os.path.isfile(pickle_fpath):
+    with open(pickle_fpath, "rb") as f:
+        saved_data = pickle.load(f)
+        datas = saved_data["datas"]
+        passes = saved_data["passes"]
+        states = saved_data["states"]
+else:
+    for ind in range(len(objs)):
+        t = sorts.equidistant_sampling(
+            orbit=objs[ind].state,
+            start_t=0,
+            end_t=end_t,
+            max_dpos=1e3,
         )
 
-        for rx in radar.rx:
-            pecef = rx.pointing_ecef / np.linalg.norm(rx.pointing_ecef, axis=0)
+        print(f"Temporal points obj {ind}: {len(t)}")
 
-            for ri in range(pecef.shape[1]):
-                point_tx = (
-                    tx.pointing_ecef / np.linalg.norm(tx.pointing_ecef, axis=0) * scanner_ctrl.r[ri]
-                    + tx.ecef
-                )
-                point = pecef[:, ri] * np.linalg.norm(rx.ecef - point_tx) + rx.ecef
+        states += [objs[ind].get_state(t)]
 
-                axes[0][0].plot(
-                    [rx.ecef[0], point[0]],
-                    [rx.ecef[1], point[1]],
-                    [rx.ecef[2], point[2]],
-                    "g-",
-                    alpha=0.05,
-                )
+        # rename cache_data to something more descriptive
+        passes += [eiscat3d.find_passes(t, states[ind], cache_data=True)]
 
-for ind in range(len(objs)):
-    for pi in range(len(passes[ind][0][0])):
-        ps = passes[ind][0][0][pi]
-        dat = datas[ind][0][0][pi]
+        data = scheduler.observe_passes(passes[ind], space_object=objs[ind], snr_limit=False)
 
-        axes[0][0].plot(
-            states[ind][0, ps.inds], states[ind][1, ps.inds], states[ind][2, ps.inds], "-"
-        )
+        datas.append(data)
 
-        if dat is not None:
-            SNRdB = 10 * np.log10(dat["snr"])
-            det_inds = SNRdB > 10.0
+    with open(pickle_fpath, "wb") as f:
+        pickle.dump({"datas": datas, "passes": passes, "states": states}, f)
 
-            axes[0][1].plot(dat["t"] / 3600.0, dat["range"] * 1e-3, "-", label=f"obj{ind}-pass{pi}")
-            axes[1][0].plot(dat["t"] / 3600.0, dat["range_rate"] * 1e-3, "-")
-            axes[1][1].plot(dat["t"] / 3600.0, SNRdB, "-")
+# fig = plt.figure(figsize=(15, 15))
+# axes = [
+#     [
+#         fig.add_subplot(221, projection="3d"),
+#         fig.add_subplot(222),
+#     ],
+#     [
+#         fig.add_subplot(223),
+#         fig.add_subplot(224),
+#     ],
+# ]
 
-            axes[0][1].plot(dat["t"][det_inds] / 3600.0, dat["range"][det_inds] * 1e-3, ".r")
-            axes[1][0].plot(dat["t"][det_inds] / 3600.0, dat["range_rate"][det_inds] * 1e-3, ".r")
-            axes[1][1].plot(dat["t"][det_inds] / 3600.0, SNRdB[det_inds], ".r")
-            axes[1][1].set_ylim([0, None])
+# sorts.plotting.grid_earth(axes[0][0])
+# for tx in eiscat3d.tx:
+#     axes[0][0].plot([tx.ecef[0]], [tx.ecef[1]], [tx.ecef[2]], "or")
+# for rx in eiscat3d.rx:
+#     axes[0][0].plot([rx.ecef[0]], [rx.ecef[1]], [rx.ecef[2]], "og")
+
+# for radar, meta in scanner_ctrl(np.arange(0, scan.cycle(), scan.dwell())):
+#     for tx in radar.tx:
+#         point_tx = (
+#             tx.pointing_ecef / np.linalg.norm(tx.pointing_ecef, axis=0) * scanner_ctrl.r.max()
+#             + tx.ecef
+#         )
+#         axes[0][0].plot(
+#             [tx.ecef[0], point_tx[0]],
+#             [tx.ecef[1], point_tx[1]],
+#             [tx.ecef[2], point_tx[2]],
+#             "r-",
+#             alpha=0.15,
+#         )
+
+#         for rx in radar.rx:
+#             pecef = rx.pointing_ecef / np.linalg.norm(rx.pointing_ecef, axis=0)
+
+#             for ri in range(pecef.shape[1]):
+#                 point_tx = (
+#                     tx.pointing_ecef / np.linalg.norm(tx.pointing_ecef, axis=0) * scanner_ctrl.r[ri]
+#                     + tx.ecef
+#                 )
+#                 point = pecef[:, ri] * np.linalg.norm(rx.ecef - point_tx) + rx.ecef
+
+#                 axes[0][0].plot(
+#                     [rx.ecef[0], point[0]],
+#                     [rx.ecef[1], point[1]],
+#                     [rx.ecef[2], point[2]],
+#                     "g-",
+#                     alpha=0.05,
+#                 )
+
+# for ind in range(len(objs)):
+#     for pi in range(len(passes[ind][0][0])):
+#         ps = passes[ind][0][0][pi]
+#         dat = datas[ind][0][0][pi]
+
+#         axes[0][0].plot(
+#             states[ind][0, ps.inds], states[ind][1, ps.inds], states[ind][2, ps.inds], "-"
+#         )
+
+#         if dat is not None:
+#             SNRdB = 10 * np.log10(dat["snr"])
+#             det_inds = SNRdB > 10.0
+
+#             axes[0][1].plot(dat["t"] / 3600.0, dat["range"] * 1e-3, "-", label=f"obj{ind}-pass{pi}")
+#             axes[1][0].plot(dat["t"] / 3600.0, dat["range_rate"] * 1e-3, "-")
+#             axes[1][1].plot(dat["t"] / 3600.0, SNRdB, "-")
+
+#             axes[0][1].plot(dat["t"][det_inds] / 3600.0, dat["range"][det_inds] * 1e-3, ".r")
+#             axes[1][0].plot(dat["t"][det_inds] / 3600.0, dat["range_rate"][det_inds] * 1e-3, ".r")
+#             axes[1][1].plot(dat["t"][det_inds] / 3600.0, SNRdB[det_inds], ".r")
+#             axes[1][1].set_ylim([0, None])
 
 
-font_ = 18
-axes[0][1].set_xlabel("Time [h]", fontsize=font_)
-axes[1][0].set_xlabel("Time [h]", fontsize=font_)
-axes[1][1].set_xlabel("Time [h]", fontsize=font_)
+# font_ = 18
+# axes[0][1].set_xlabel("Time [h]", fontsize=font_)
+# axes[1][0].set_xlabel("Time [h]", fontsize=font_)
+# axes[1][1].set_xlabel("Time [h]", fontsize=font_)
 
-axes[0][1].set_ylabel("Two way range [km]", fontsize=font_)
-axes[1][0].set_ylabel("Two way range rate [km/s]", fontsize=font_)
-axes[1][1].set_ylabel("SNR [dB]", fontsize=font_)
+# axes[0][1].set_ylabel("Two way range [km]", fontsize=font_)
+# axes[1][0].set_ylabel("Two way range rate [km/s]", fontsize=font_)
+# axes[1][1].set_ylabel("SNR [dB]", fontsize=font_)
 
-axes[0][1].legend()
+# axes[0][1].legend()
 
-dr = 600e3
-axes[0][0].set_xlim([eiscat3d.tx[0].ecef[0] - dr, eiscat3d.tx[0].ecef[0] + dr])
-axes[0][0].set_ylim([eiscat3d.tx[0].ecef[1] - dr, eiscat3d.tx[0].ecef[1] + dr])
-axes[0][0].set_zlim([eiscat3d.tx[0].ecef[2] - dr, eiscat3d.tx[0].ecef[2] + dr])
+# dr = 600e3
+# axes[0][0].set_xlim([eiscat3d.tx[0].ecef[0] - dr, eiscat3d.tx[0].ecef[0] + dr])
+# axes[0][0].set_ylim([eiscat3d.tx[0].ecef[1] - dr, eiscat3d.tx[0].ecef[1] + dr])
+# axes[0][0].set_zlim([eiscat3d.tx[0].ecef[2] - dr, eiscat3d.tx[0].ecef[2] + dr])
 
-plt.show()
+# plt.show()
