8f6d4386335717c53d78727c95fc7cf41a449541
tszhinh <tsz-hin.heung@irf.se>
2025-04-15 16:00:11 +0200

    fixed 'simulate_scanning.py' example in 'simulate_scanning_v2.py'

--- examples/simulate_scanning_v2.py
+++ examples/simulate_scanning_v2.py
@@ -101,93 +101,94 @@ else:
     with open(pickle_fpath, "wb") as f:
         pickle.dump({"datas": datas, "passes": passes, "states": states}, f)
 
-# fig = plt.figure(figsize=(15, 15))
-# axes = [
-#     [
-#         fig.add_subplot(221, projection="3d"),
-#         fig.add_subplot(222),
-#     ],
-#     [
-#         fig.add_subplot(223),
-#         fig.add_subplot(224),
-#     ],
-# ]
+fig = plt.figure(figsize=(15, 15))
+axes = [
+    [
+        fig.add_subplot(221, projection="3d"),
+        fig.add_subplot(222),
+    ],
+    [
+        fig.add_subplot(223),
+        fig.add_subplot(224),
+    ],
+]
 
-# sorts.plotting.grid_earth(axes[0][0])
-# for tx in eiscat3d.tx:
-#     axes[0][0].plot([tx.ecef[0]], [tx.ecef[1]], [tx.ecef[2]], "or")
-# for rx in eiscat3d.rx:
-#     axes[0][0].plot([rx.ecef[0]], [rx.ecef[1]], [rx.ecef[2]], "og")
+sorts.plotting.grid_earth(axes[0][0])
+for tx in eiscat3d.tx:
+    axes[0][0].plot([tx.ecef[0]], [tx.ecef[1]], [tx.ecef[2]], "or")
+for rx in eiscat3d.rx:
+    axes[0][0].plot([rx.ecef[0]], [rx.ecef[1]], [rx.ecef[2]], "og")
 
-# for radar, meta in scanner_ctrl(np.arange(0, scan.cycle(), scan.dwell())):
-#     for tx in radar.tx:
-#         point_tx = (
-#             tx.pointing_ecef / np.linalg.norm(tx.pointing_ecef, axis=0) * scanner_ctrl.r.max()
-#             + tx.ecef
-#         )
-#         axes[0][0].plot(
-#             [tx.ecef[0], point_tx[0]],
-#             [tx.ecef[1], point_tx[1]],
-#             [tx.ecef[2], point_tx[2]],
-#             "r-",
-#             alpha=0.15,
-#         )
+radar_meta_pairs = list(scanner_ctrl(np.arange(0, scan.cycle(), scan.dwell())))
+for radar, meta in radar_meta_pairs:
+    for tx in radar.tx:
+        point_tx = (
+            tx.pointing_ecef / np.linalg.norm(tx.pointing_ecef, axis=0) * scanner_ctrl.r.max()
+        ).flatten() + tx.ecef
 
-#         for rx in radar.rx:
-#             pecef = rx.pointing_ecef / np.linalg.norm(rx.pointing_ecef, axis=0)
+        axes[0][0].plot(
+            [tx.ecef[0], point_tx[0]],
+            [tx.ecef[1], point_tx[1]],
+            [tx.ecef[2], point_tx[2]],
+            "r-",
+            alpha=0.15,
+        )
 
-#             for ri in range(pecef.shape[1]):
-#                 point_tx = (
-#                     tx.pointing_ecef / np.linalg.norm(tx.pointing_ecef, axis=0) * scanner_ctrl.r[ri]
-#                     + tx.ecef
-#                 )
-#                 point = pecef[:, ri] * np.linalg.norm(rx.ecef - point_tx) + rx.ecef
+        for rx in radar.rx:
+            pecef = rx.pointing_ecef / np.linalg.norm(rx.pointing_ecef, axis=0)
 
-#                 axes[0][0].plot(
-#                     [rx.ecef[0], point[0]],
-#                     [rx.ecef[1], point[1]],
-#                     [rx.ecef[2], point[2]],
-#                     "g-",
-#                     alpha=0.05,
-#                 )
+            for ri in range(pecef.shape[1]):
+                point_tx = (
+                    tx.pointing_ecef / np.linalg.norm(tx.pointing_ecef, axis=0) * scanner_ctrl.r[ri]
+                    + tx.ecef
+                )
+                point = pecef[:, ri] * np.linalg.norm(rx.ecef - point_tx) + rx.ecef
 
-# for ind in range(len(objs)):
-#     for pi in range(len(passes[ind][0][0])):
-#         ps = passes[ind][0][0][pi]
-#         dat = datas[ind][0][0][pi]
+                axes[0][0].plot(
+                    [rx.ecef[0], point[0]],
+                    [rx.ecef[1], point[1]],
+                    [rx.ecef[2], point[2]],
+                    "g-",
+                    alpha=0.05,
+                )
 
-#         axes[0][0].plot(
-#             states[ind][0, ps.inds], states[ind][1, ps.inds], states[ind][2, ps.inds], "-"
-#         )
+for ind in range(len(objs)):
+    for pi in range(len(passes[ind][0][0])):
+        ps = passes[ind][0][0][pi]
+        dat = datas[ind][0][0][pi]
 
-#         if dat is not None:
-#             SNRdB = 10 * np.log10(dat["snr"])
-#             det_inds = SNRdB > 10.0
+        axes[0][0].plot(
+            states[ind][0, ps.inds], states[ind][1, ps.inds], states[ind][2, ps.inds], "-"
+        )
 
-#             axes[0][1].plot(dat["t"] / 3600.0, dat["range"] * 1e-3, "-", label=f"obj{ind}-pass{pi}")
-#             axes[1][0].plot(dat["t"] / 3600.0, dat["range_rate"] * 1e-3, "-")
-#             axes[1][1].plot(dat["t"] / 3600.0, SNRdB, "-")
+        if dat is not None:
+            SNRdB = 10 * np.log10(dat["snr"])
+            det_inds = SNRdB > 10.0
 
-#             axes[0][1].plot(dat["t"][det_inds] / 3600.0, dat["range"][det_inds] * 1e-3, ".r")
-#             axes[1][0].plot(dat["t"][det_inds] / 3600.0, dat["range_rate"][det_inds] * 1e-3, ".r")
-#             axes[1][1].plot(dat["t"][det_inds] / 3600.0, SNRdB[det_inds], ".r")
-#             axes[1][1].set_ylim([0, None])
+            axes[0][1].plot(dat["t"] / 3600.0, dat["range"] * 1e-3, "-", label=f"obj{ind}-pass{pi}")
+            axes[1][0].plot(dat["t"] / 3600.0, dat["range_rate"] * 1e-3, "-")
+            axes[1][1].plot(dat["t"] / 3600.0, SNRdB, "-")
+
+            axes[0][1].plot(dat["t"][det_inds] / 3600.0, dat["range"][det_inds] * 1e-3, ".r")
+            axes[1][0].plot(dat["t"][det_inds] / 3600.0, dat["range_rate"][det_inds] * 1e-3, ".r")
+            axes[1][1].plot(dat["t"][det_inds] / 3600.0, SNRdB[det_inds], ".r")
+            axes[1][1].set_ylim([0, None])
 
 
-# font_ = 18
-# axes[0][1].set_xlabel("Time [h]", fontsize=font_)
-# axes[1][0].set_xlabel("Time [h]", fontsize=font_)
-# axes[1][1].set_xlabel("Time [h]", fontsize=font_)
+font_ = 18
+axes[0][1].set_xlabel("Time [h]", fontsize=font_)
+axes[1][0].set_xlabel("Time [h]", fontsize=font_)
+axes[1][1].set_xlabel("Time [h]", fontsize=font_)
 
-# axes[0][1].set_ylabel("Two way range [km]", fontsize=font_)
-# axes[1][0].set_ylabel("Two way range rate [km/s]", fontsize=font_)
-# axes[1][1].set_ylabel("SNR [dB]", fontsize=font_)
+axes[0][1].set_ylabel("Two way range [km]", fontsize=font_)
+axes[1][0].set_ylabel("Two way range rate [km/s]", fontsize=font_)
+axes[1][1].set_ylabel("SNR [dB]", fontsize=font_)
 
-# axes[0][1].legend()
+axes[0][1].legend()
 
-# dr = 600e3
-# axes[0][0].set_xlim([eiscat3d.tx[0].ecef[0] - dr, eiscat3d.tx[0].ecef[0] + dr])
-# axes[0][0].set_ylim([eiscat3d.tx[0].ecef[1] - dr, eiscat3d.tx[0].ecef[1] + dr])
-# axes[0][0].set_zlim([eiscat3d.tx[0].ecef[2] - dr, eiscat3d.tx[0].ecef[2] + dr])
+dr = 600e3
+axes[0][0].set_xlim([eiscat3d.tx[0].ecef[0] - dr, eiscat3d.tx[0].ecef[0] + dr])
+axes[0][0].set_ylim([eiscat3d.tx[0].ecef[1] - dr, eiscat3d.tx[0].ecef[1] + dr])
+axes[0][0].set_zlim([eiscat3d.tx[0].ecef[2] - dr, eiscat3d.tx[0].ecef[2] + dr])
 
-# plt.show()
+plt.show()
