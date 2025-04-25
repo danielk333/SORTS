1d27dd1d33cf01b8019af29d9eee2d46699de19f
tszhinh <tsz-hin.heung@irf.se>
2025-04-25 18:11:22 +0200

    save some calculation of 'calculate_simple_stx_srx_observations_should_matches_simulate_scanning_v2_example_test' by pickle to same re-calc time

--- src/sorts/detection_config_test.py
+++ src/sorts/detection_config_test.py
@@ -181,116 +181,118 @@ def calculate_simple_stx_srx_observations_smoke_test():
 def calculate_simple_stx_srx_observations_should_matches_simulate_scanning_v2_example_test():
     """following the same params as in `examples/examples/simulate_scanning_v2.py`"""
 
-    spobj_smpl_dt_arr = sorts.equidistant_sampling(
-        orbit=spobj.state,
-        start_t=0,
-        end_t=simulation_end_dt,
-        max_dpos=1e3,
+    sim_result_fpath = (
+        Path(os.path.dirname(os.path.abspath(__file__)))
+        / f"{calculate_simple_stx_srx_observations_should_matches_simulate_scanning_v2_example_test.__name__}.pickle"
     )
+    if os.path.isfile(sim_result_fpath):
+        with open(sim_result_fpath, "rb") as f:
+            saved_data = pickle.load(f)
+            dcfg = saved_data["dcfg"]
+            # spobj = saved_data["spobj"] # skipped, it is a global of test file atm
+            # epoch = saved_data["epoch"] # skipped, it is a global of test file atm
+    else:
+        spobj_smpl_dt_arr = sorts.equidistant_sampling(
+            orbit=spobj.state,
+            start_t=0,
+            end_t=simulation_end_dt,
+            max_dpos=1e3,
+        )
 
-    tx_station = eiscat3d.tx[0]
-    rx_station = eiscat3d.rx[0]
+        exp_detail = ExperimentDetail(1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0)
+        exp_num = 0
+        exp_num_map: dict[int, ExperimentDetail] = {0: exp_detail}
 
-    exp_detail = ExperimentDetail(1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0)
-    exp_num = 0
-    exp_num_map: dict[int, ExperimentDetail] = {0: exp_detail}
+        # TODO: feels like we could use a tracker controller here
+        #   (`src/sorts/controller_v2/tracker_controller.py`),
+        #   but it requires a `Pass` object which is not that convenient to use
+        #     - a `Pass` object feels like sth internal to a simulation and not sth known in prior/externally
+        spobj_smpl_states = spobj.get_state(spobj_smpl_dt_arr)
 
-    # TODO: feels like we could use a tracker controller here
-    #   (`src/sorts/controller_v2/tracker_controller.py`),
-    #   but it requires a `Pass` object which is not that convenient to use
-    #     - a `Pass` object feels like sth internal to a simulation and not sth known in prior/externally
-    spobj_smpl_states = spobj.get_state(spobj_smpl_dt_arr)
+        pass_arr = eiscat3d.find_passes(spobj_smpl_dt_arr, spobj_smpl_states, cache_data=True)
+        target_pass_obj: sorts.Pass = pass_arr[0][0][0]
 
-    pass_arr = eiscat3d.find_passes(spobj_smpl_dt_arr, spobj_smpl_states, cache_data=True)
-    target_pass_obj: sorts.Pass = pass_arr[0][0][0]
+        scan = sorts.radar.scans.Fence(azimuth=90, num=40, dwell=0.1, min_elevation=30)
+        scan_dt_arr = np.arange(0, simulation_end_dt, scan.dwell())
+        scanner_ctrl = sorts.controller.Scanner(
+            eiscat3d,
+            scan,
+            t=scan_dt_arr,
+        )
+        scan_dt_arr_pass_mask = np.logical_and(
+            scan_dt_arr >= target_pass_obj.t[0], scan_dt_arr <= target_pass_obj.t[-1]
+        )
+        scan_dt_arr_pass = scan_dt_arr[scan_dt_arr_pass_mask]
+        sch_total_rows = len(scan_dt_arr_pass)
 
-    scan = sorts.radar.scans.Fence(azimuth=90, num=40, dwell=0.1, min_elevation=30)
-    scan_dt_arr = np.arange(0, simulation_end_dt, scan.dwell())
-    scanner_ctrl = sorts.controller.Scanner(
-        eiscat3d,
-        scan,
-        t=scan_dt_arr,
-    )
-    scan_dt_arr_pass_mask = np.logical_and(
-        scan_dt_arr >= target_pass_obj.t[0], scan_dt_arr <= target_pass_obj.t[-1]
-    )
-    scan_dt_arr_pass = scan_dt_arr[scan_dt_arr_pass_mask]
-    sch_total_rows = len(scan_dt_arr_pass)
+        # space object in tx, rx station coordinate and the pointings of tx, rx station,
+        # all under the delta times of a pass
+        # note that `tx_azelr_deg_pass`, `rx_azelr_deg_pass` init to 1.0,
+        # TODO: vectorize
+        spobj_tx_enu_pass = np.full((6, sch_total_rows), 0, dtype=np.float64)
+        spobj_rx_enu_pass = np.full((6, sch_total_rows), 0, dtype=np.float64)
+        tx_azelr_deg_pass = np.full((3, sch_total_rows), 1.0, dtype=np.float64)
+        rx_azelr_deg_pass = np.full((3, sch_total_rows), 1.0, dtype=np.float64)
+        for dt_idx, (radar, _meta) in enumerate(scanner_ctrl.generator(scan_dt_arr_pass)):
+            spobj_tx_enu_pass[:, dt_idx] = radar.tx[0].enu(
+                spobj.get_state([scan_dt_arr_pass[dt_idx]])
+            )[:, 0]
+            spobj_rx_enu_pass[:, dt_idx] = radar.rx[0].enu(
+                spobj.get_state([scan_dt_arr_pass[dt_idx]])
+            )[:, 0]
+            tx_azelr_deg_pass[:2, dt_idx] = [
+                radar.tx[0].beam.azimuth[0],
+                radar.tx[0].beam.elevation[0],
+            ]
+            rx_azelr_deg_pass[:2, dt_idx] = [
+                radar.rx[0].beam.azimuth[0],
+                radar.rx[0].beam.elevation[0],
+            ]
 
-    # space object in tx, rx station coordinate and the pointings of tx, rx station,
-    # all under the delta times of a pass
-    # note that `tx_azelr_deg_pass`, `rx_azelr_deg_pass` init to 1.0,
-    # TODO: vectorize
-    spobj_tx_enu_pass = np.full((6, sch_total_rows), 0, dtype=np.float64)
-    spobj_rx_enu_pass = np.full((6, sch_total_rows), 0, dtype=np.float64)
-    tx_azelr_deg_pass = np.full((3, sch_total_rows), 1.0, dtype=np.float64)
-    rx_azelr_deg_pass = np.full((3, sch_total_rows), 1.0, dtype=np.float64)
-    for dt_idx, (radar, _meta) in enumerate(scanner_ctrl.generator(scan_dt_arr_pass)):
-        spobj_tx_enu_pass[:, dt_idx] = radar.tx[0].enu(spobj.get_state([scan_dt_arr_pass[dt_idx]]))[
-            :, 0
-        ]
-        spobj_rx_enu_pass[:, dt_idx] = radar.rx[0].enu(spobj.get_state([scan_dt_arr_pass[dt_idx]]))[
-            :, 0
-        ]
-        tx_azelr_deg_pass[:2, dt_idx] = [radar.tx[0].beam.azimuth[0], radar.tx[0].beam.elevation[0]]
-        rx_azelr_deg_pass[:2, dt_idx] = [radar.rx[0].beam.azimuth[0], radar.rx[0].beam.elevation[0]]
+        scan_pass_stt_tstmp_us = (scan_dt_arr_pass * 1e6).astype("timedelta64[us]") + np.datetime64(
+            t.cast(datetime, epoch.to_datetime(timezone=timezone.utc))
+        )
 
-    scan_pass_stt_tstmp_us = (scan_dt_arr_pass * 1e6).astype("timedelta64[us]") + np.datetime64(
-        t.cast(datetime, epoch.to_datetime(timezone=timezone.utc))
-    )
+        tx_schedule = Schedule(
+            stt_tstmp_us=scan_pass_stt_tstmp_us,
+            exp_num=np.full(sch_total_rows, exp_num),
+            pointing_az=tx_azelr_deg_pass[0],
+            pointing_el=tx_azelr_deg_pass[1],
+            coh_int_bandwidth=np.full(sch_total_rows, exp_detail.coh_int_bandwidth),
+            ipp=np.full(sch_total_rows, exp_detail.ipp),
+            pulse_length=np.full(sch_total_rows, exp_detail.pulse_length),
+        )
 
-    tx_schedule = Schedule(
-        stt_tstmp_us=scan_pass_stt_tstmp_us,
-        exp_num=np.full(sch_total_rows, exp_num),
-        pointing_az=tx_azelr_deg_pass[0],
-        pointing_el=tx_azelr_deg_pass[1],
-        coh_int_bandwidth=np.full(sch_total_rows, exp_detail.coh_int_bandwidth),
-        ipp=np.full(sch_total_rows, exp_detail.ipp),
-        pulse_length=np.full(sch_total_rows, exp_detail.pulse_length),
-    )
+        rx_schedule = Schedule(
+            stt_tstmp_us=scan_pass_stt_tstmp_us,
+            exp_num=np.full(sch_total_rows, exp_num),
+            pointing_az=rx_azelr_deg_pass[0],
+            pointing_el=rx_azelr_deg_pass[1],
+            coh_int_bandwidth=np.full(sch_total_rows, exp_detail.coh_int_bandwidth),
+            ipp=np.full(sch_total_rows, exp_detail.ipp),
+            pulse_length=np.full(sch_total_rows, exp_detail.pulse_length),
+        )
 
-    rx_schedule = Schedule(
-        stt_tstmp_us=scan_pass_stt_tstmp_us,
-        exp_num=np.full(sch_total_rows, exp_num),
-        pointing_az=rx_azelr_deg_pass[0],
-        pointing_el=rx_azelr_deg_pass[1],
-        coh_int_bandwidth=np.full(sch_total_rows, exp_detail.coh_int_bandwidth),
-        ipp=np.full(sch_total_rows, exp_detail.ipp),
-        pulse_length=np.full(sch_total_rows, exp_detail.pulse_length),
-    )
+        dcfg = SimpleStxSrx(
+            stt_tstmp_us=scan_pass_stt_tstmp_us,
+            tx_station=eiscat3d.tx[0],
+            rx_station=eiscat3d.rx[0],
+            tx_schedule=tx_schedule,
+            rx_schedule=rx_schedule,
+            exp_num_map=exp_num_map,
+        )
 
-    dcfg = SimpleStxSrx(
-        stt_tstmp_us=scan_pass_stt_tstmp_us,
-        tx_station=eiscat3d.tx[0],
-        rx_station=eiscat3d.rx[0],
-        tx_schedule=tx_schedule,
-        rx_schedule=rx_schedule,
-        exp_num_map=exp_num_map,
-    )
-
-    # sim_result_fpath = (
-    #     Path(os.path.dirname(os.path.abspath(__file__)))
-    #     / f"{calculate_simple_stx_srx_observations_should_matches_simulate_scanning_v2_example_test.__name__}.pickle"
-    # )
-    # if os.path.isfile(sim_result_fpath):
-    #     with open(sim_result_fpath, "rb") as f:
-    #         saved_data = pickle.load(f)
-    #         dcfg = saved_data["dcfg"]
-    #         spobj = saved_data["spobj"]
-    #         epoch = saved_data["epoch"]
-    # else:
-    #     ...  # TODO: imple
-    #     # with open(sim_result_fpath, "wb") as f:
-    #     #     pickle.dump(
-    #     #         {
-    #     #             "scan_dt_arr_pass": scan_dt_arr_pass,
-    #     #             "dcfg": dcfg,
-    #     #             "spobj": spobj,
-    #     #             "epoch": epoch,
-    #     #             "obs": None,
-    #     #         },
-    #     #         f,
-    #     #     )
+        with open(sim_result_fpath, "wb") as f:
+            pickle.dump(
+                {
+                    "scan_dt_arr_pass": scan_dt_arr_pass,
+                    "dcfg": dcfg,
+                    # "spobj": spobj, # skipped, it is a global of test file atm
+                    # "epoch": epoch, # skipped, it is a global of test file atm
+                    "obs": None,
+                },
+                f,
+            )
 
     obs = calculate_simple_stx_srx_observations(
         dcfg=dcfg,
