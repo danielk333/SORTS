961416ba43fc65bc08686952b55aa1c323027d83
tszhinh <tsz-hin.heung@irf.se>
2025-05-08 17:33:09 +0200

    remove 'ScannerController'

--- src/sorts/controller_v2/controller.py
+++ src/sorts/controller_v2/controller.py
@@ -2,14 +2,12 @@ import typing as t
 
 from .random_uniform_scans_controller import RandomUniformScansController
 from .tracker_controller import TrackerController
-from .scanner_controller import ScannerController
 from .fence_scan_controller import FenceScanController
 
 
 Controller = t.Union[
     RandomUniformScansController,
     TrackerController,
-    ScannerController,
     FenceScanController,
 ]
 
