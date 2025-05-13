9edb24f0df75d23d18c8c5fb58e0c6c111433ddd
tszhinh <tsz-hin.heung@irf.se>
2025-05-13 17:24:39 +0200

    breaking the ref to 'passes' module in 'detection_config' by having a local verion of 'find_simultaneous_passes_time_ranges'

--- src/sorts/detection_config/detection_config.py
+++ src/sorts/detection_config/detection_config.py
@@ -8,9 +8,7 @@ import sorts
 from sorts.radar.radars.composite_key import RadarStationCompositeKey
 from sorts.radar.tx_rx import Station
 from sorts.types import Datetime64_us, Float_64_as_sec, Float64_as_m, EcefStates
-from sorts import passes_v2 as passes
 from sorts import scheduler_v2 as scheduler
-from sorts import controller_v2 as controller
 from sorts.detection_config import ExperimentDetail, Observation
 
 
@@ -79,7 +77,7 @@ class StxSrx(DetectionConfigProtocol):
         space_object_states: EcefStates,
         epoch: datetime,
     ):
-        time_ranges = passes.find_simultaneous_passes_time_ranges(
+        time_ranges = find_simultaneous_passes_time_ranges(
             dt_s_arr=dt_s_arr,
             states=space_object_states,
             stations=[self.tx_station, self.rx_station],
@@ -325,6 +323,60 @@ def calculate_simple_stx_srx_observations(
     )
 
     return obs
+
+
+def find_simultaneous_passes_time_ranges(
+    dt_s_arr: npt.NDArray[np.float64],
+    states: EcefStates,
+    stations: list[Station],
+    epoch: datetime,
+    fov_kw=None,
+) -> t.Sequence[tuple[Datetime64_us, Datetime64_us]]:
+    """
+    Finds all passes that are simultaneously inside a multiple stations FOV's.
+    """
+
+    time_ranges: list[tuple[Datetime64_us, Datetime64_us]] = []
+    if fov_kw is None:
+        fov_kw = {}
+
+    enu = []
+    check = np.full((len(dt_s_arr),), True, dtype=bool)
+    for station in stations:
+        enu_st = station.enu(states)
+        enu.append(enu_st)
+
+        check_st = station.field_of_view(states, **fov_kw)
+        check = np.logical_and(check, check_st)
+
+    inds = np.where(check)[0]
+
+    if len(inds) == 0:
+        return time_ranges
+
+    dind = np.diff(inds)
+    splits = np.where(dind > 1)[0]
+
+    splits = np.insert(splits, 0, -1)
+    splits = np.insert(splits, len(splits), len(inds) - 1)
+    splits += 1
+    for si in range(len(splits) - 1):
+        ps_inds = inds[splits[si] : splits[si + 1]]
+        if len(ps_inds) == 0:
+            continue
+
+        start_time: Datetime64_us = t.cast(
+            np.timedelta64, (dt_s_arr[splits[si]] * 1e6).astype("timedelta64[us]")
+        ) + np.datetime64(epoch)
+
+        end_time: Datetime64_us = t.cast(
+            np.timedelta64, (dt_s_arr[splits[si + 1]] * 1e6).astype("timedelta64[us]")
+        ) + np.datetime64(epoch)
+
+        time_range = (start_time, end_time)
+        time_ranges.append(time_range)
+
+    return time_ranges
 
 
 __all__ = [
