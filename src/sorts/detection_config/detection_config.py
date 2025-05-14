78cc0741c88902595a357e91fbec00ab75677ee0
tszhinh <tsz-hin.heung@irf.se>
2025-05-14 11:18:08 +0200

    - removed 'SimpleStxSrx' detection config - removed old detection config test and example

--- src/sorts/detection_config/detection_config.py
+++ src/sorts/detection_config/detection_config.py
@@ -29,38 +29,6 @@ class DetectionConfigProtocol(t.Protocol):
     ) -> Observation: ...
 
 
-@dataclass
-class SimpleStxSrx(DetectionConfigProtocol):
-    stt_tstmp_us: npt.NDArray[np.datetime64]
-    "the timestamp which observations can be made"
-
-    tx_station: Station
-    rx_station: Station
-
-    tx_schedule: scheduler.Schedule
-    "the tx radar station schedule, can contain more entries than `stt_tstmp_us`"
-    rx_schedule: scheduler.Schedule
-    "the rx radar station schedule, can contain more entries than `stt_tstmp_us`"
-
-    exp_num_map: dict[int, ExperimentDetail]
-
-    # TODO: implement or remove
-    def find_passes_time_ranges(
-        self,
-        dt_s_arr: npt.NDArray[np.float64],
-        space_object_states: npt.NDArray[np.float64],
-        epoch: datetime,
-    ) -> t.Sequence[tuple[Datetime64_us, Datetime64_us]]: ...
-
-    # TODO: implement or remove
-    def calculate_observation(
-        self,
-        space_object: sorts.SpaceObject,
-        epoch: datetime,
-        schedule_mask: npt.NDArray[np.bool] | None,
-    ) -> Observation: ...
-
-
 # TODO: dissolve existing `SimpleStxSrx` and rename this to `SimpleStxSrx`
 @dataclass(kw_only=True)
 class StxSrx(DetectionConfigProtocol):
@@ -236,93 +204,7 @@ class StxMrx(DetectionConfigProtocol):
     ) -> Observation: ...
 
 
-DetectionConfig: t.TypeAlias = t.Union[SimpleStxSrx, StxSrx, StxMrx]
-
-
-# TODO: should move to other file? e.g. calculations?
-# TODO: should vectorize and work against an array of SpaceObject?
-def calculate_simple_stx_srx_observations(
-    dcfg: SimpleStxSrx, space_object: sorts.SpaceObject, epoch: datetime
-) -> Observation:
-    dt_arr: npt.NDArray[Float_64_as_sec] = (
-        (dcfg.stt_tstmp_us - np.datetime64(epoch)).astype("timedelta64[us]").astype(np.float64)
-    ) * 1e-6
-    states = space_object.get_state(dt_arr)
-
-    space_object_tx_enu = dcfg.tx_station.enu(states)  # space object in tx station coordinate
-    space_object_rx_enu = dcfg.rx_station.enu(states)  # space object in rx station coordinate
-
-    range_tx_m = np.linalg.norm(space_object_tx_enu[:3, :], axis=0)
-    range_rx_m = np.linalg.norm(space_object_rx_enu[:3, :], axis=0)
-
-    snr = np.empty((len(dcfg.stt_tstmp_us),), dtype=np.float64)
-    rcs = np.empty((len(dcfg.stt_tstmp_us),), dtype=np.float64)
-
-    powers = np.empty((len(dcfg.stt_tstmp_us),), dtype=np.float64)
-    t_slices = np.empty((len(dcfg.stt_tstmp_us),), dtype=np.float64)
-    txrx_on = np.full((len(dcfg.stt_tstmp_us),), False, dtype=bool)
-
-    pulse_lengths = np.array(
-        [dcfg.exp_num_map[n].pulse_length for n in dcfg.tx_schedule.exp_num], dtype=np.float64
-    )
-    ipps = np.array([dcfg.exp_num_map[n].ipp for n in dcfg.tx_schedule.exp_num], dtype=np.float64)
-    powers = np.array(
-        [dcfg.exp_num_map[n].power for n in dcfg.tx_schedule.exp_num], dtype=np.float64
-    )
-    bandwidths = np.array(
-        [dcfg.exp_num_map[n].bandwidth for n in dcfg.tx_schedule.exp_num], dtype=np.float64
-    )
-    duty_cycles = np.array(
-        [dcfg.exp_num_map[n].duty_cycle for n in dcfg.tx_schedule.exp_num], dtype=np.float64
-    )
-    rx_noise_temps = np.array(
-        [dcfg.exp_num_map[n].noise_temp for n in dcfg.tx_schedule.exp_num], dtype=np.float64
-    )
-
-    # TODO: vectorize
-    tx_gain_arr = np.full(len(dcfg.stt_tstmp_us), 0.0, dtype=np.float64)
-    rx_gain_arr = np.full(len(dcfg.stt_tstmp_us), 0.0, dtype=np.float64)
-    for idx, _ in enumerate(dcfg.stt_tstmp_us):
-        dcfg.tx_station.beam.sph_point(
-            dcfg.tx_schedule.pointing_az[idx], dcfg.tx_schedule.pointing_el[idx], degrees=True
-        )
-        tx_gain_arr[idx] = dcfg.tx_station.beam.gain(space_object_tx_enu[:3, idx])
-
-        dcfg.rx_station.beam.sph_point(
-            dcfg.rx_schedule.pointing_az[idx], dcfg.tx_schedule.pointing_el[idx], degrees=True
-        )
-        rx_gain_arr[idx] = dcfg.rx_station.beam.gain(space_object_rx_enu[:3, idx])
-
-    tx_wavelength: float = dcfg.tx_station.beam.wavelength
-    rx_wavelength: float = dcfg.rx_station.beam.wavelength
-
-    snr = sorts.signals.hard_target_snr(
-        tx_gain_arr,
-        rx_gain_arr,
-        tx_wavelength,
-        powers[0],  # TODO: improve: hard-coded from `exp_detail`
-        range_tx_m,
-        range_rx_m,
-        diameter=space_object.d,
-        bandwidth=bandwidths[0],  # TODO: improve: hard-coded from `exp_detail`
-        rx_noise_temp=rx_noise_temps[0],  # TODO: improve: hard-coded from `exp_detail`
-        radar_albedo=space_object.parameters.get("radar_albedo", 1.0),
-    )
-
-    # TODO: add `doppler_spread_integrated_snr:` support
-    # TODO: add `blind_ranges:` support
-
-    obs = Observation(
-        snr=snr,
-        range=range_tx_m + range_rx_m,
-        range_rx=range_rx_m,
-        range_rate=np.full(len(dcfg.stt_tstmp_us), 1.0, dtype=np.float64),  # TODO: imple
-        tx_k=space_object_tx_enu[:3] / range_tx_m,
-        rx_k=space_object_rx_enu[:3] / range_rx_m,
-        rcs=np.full(len(dcfg.stt_tstmp_us), 1.0, dtype=np.float64),  # TODO: imple
-    )
-
-    return obs
+DetectionConfig: t.TypeAlias = t.Union[StxSrx, StxMrx]
 
 
 def find_simultaneous_passes_time_ranges(
@@ -381,10 +263,8 @@ def find_simultaneous_passes_time_ranges(
 
 __all__ = [
     "DetectionConfigProtocol",
-    "SimpleStxSrx",
     "StxSrx",
     "StxMrx",
     "DetectionConfig",
-    "calculate_simple_stx_srx_observations",
     "find_simultaneous_passes_time_ranges",
 ]
