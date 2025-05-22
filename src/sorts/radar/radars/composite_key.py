feb3db4b362ce96db0ed53b5c9d719a4d2e05e58
tszhinh <tsz-hin.heung@irf.se>
2025-05-22 17:29:35 +0200

    mark 'composite_key.py' as potentially removable

--- src/sorts/radar/radars/composite_key.py
+++ src/sorts/radar/radars/composite_key.py
@@ -1,26 +1,19 @@
 import typing as t
 
 
-# TODO: relocate and integrate with `Radar` class
+# TODO: remove?
 RadarCompositeKey = tuple[str, ...]
 """
 a structed unique identifer for a radar.
 
 currently the tuple corresponds to the first 2 param of `radars.get_radar()`,
 with slightly tightened typing
-
-WIP
-
-TODO: discuss with Daniel if tuple of strings are good enough ids
 """
 
+# TODO: remove?
 RadarStationCompositeKey = tuple[str, ...]
 """
 a structed unique identifer for a station of a radar.
 
 similar to `RadarCompositeKey` but for radar station
-
-WIP
-
-TODO: discuss with Daniel if tuple of strings are good enough ids
 """
