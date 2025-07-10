7f414adb50f31e76867d0bd896cbd2b5ee7cac22
tszhinh <tsz-hin.heung@irf.se>
2025-07-10 17:29:38 +0200

    - defined 'DataFrameColumnNames' as a class - include the generated column 'end_time' in method 'as_dataframe' - stop using "start time" as the dataframe index for 'Scheule'

--- src/sorts/schedule_v2.py
+++ src/sorts/schedule_v2.py
@@ -9,6 +9,39 @@ from sorts.types import Timedelta64_us, Datetime64_us, Float64_as_deg
 logger = logging.getLogger(__name__)
 
 
+class DataFrameColumnNames:
+    """
+    Define the `pandas` `DataFrame` column names of a `Schedule` as class member.
+    """
+
+    # TODO: add test to ensure this file up-to-date with `Schedule class
+
+    start_time: t.Final = "stt_tstmp_us"
+    end_time: t.Final = "end_time"
+    pointing_az: t.Final = "pointing_az"
+    pointing_el: t.Final = "pointing_el"
+    exp_num: t.Final = "exp_num"
+
+    @classmethod
+    def all(cls) -> list[str]:
+        """Return a list of all column names."""
+
+        return [
+            t.cast(str, v)
+            for k, v in vars(DataFrameColumnNames).items()
+            if (not k.startswith("__")) and (not isinstance(v, classmethod))
+        ]
+
+    @classmethod
+    def all_non_derived(cls) -> list[str]:
+        """
+        Return a list of all non-derived column names.
+        (i.e. It is not generated and has a corresponding field in the `Schedule` dataclass)
+        """
+
+        return [c for c in cls.all() if c not in [cls.end_time]]
+
+
 # TODO: rename to sth like `ControlSliceDetail`?
 @dataclass(kw_only=True)
 class ExperimentDetail:
@@ -62,29 +95,19 @@ class Schedule:
         return sch
 
     @classmethod
-    def metadata_fields(cls):
-        return (f for f in fields(cls) if f.name == "meta")
-
-    @classmethod
-    def non_metadata_fields(cls):
-        return (f for f in fields(cls) if not f.name == "meta")
-
-    @classmethod
     def from_dataframe(cls, df: pd.DataFrame, meta: dict[int, ExperimentDetail]) -> Schedule:
-        df = df.reset_index()  # put df index back into a df column
-        sch_dict = {
-            f.name: df[f.name].to_numpy()
-            for f in Schedule.non_metadata_fields()
-            if not f.name.startswith("_")
-        }
+        sch_dict = {c: df[c].to_numpy() for c in DataFrameColumnNames.all_non_derived()}
         sch = Schedule(**sch_dict, meta=meta)
 
         return sch
 
     def __post_init__(self):
-        f_0, *f_rests = Schedule.non_metadata_fields()
+        f_0, *f_rests = [
+            f for f in fields(Schedule) if f.name in DataFrameColumnNames.all_non_derived()
+        ]
         fv_0, *fv_rests = t.cast(
-            tuple[npt.NDArray, ...], [getattr(self, f.name) for f in Schedule.non_metadata_fields()]
+            tuple[npt.NDArray, ...],
+            [getattr(self, c) for c in DataFrameColumnNames.all_non_derived()],
         )  # actual value of the fields
 
         for idx, f in enumerate(fv_rests):
@@ -95,11 +118,27 @@ class Schedule:
                     f"while shape of {f_0.name} is {fv_0.shape} "
                 )
 
-    def as_dataframe(self) -> pd.DataFrame:
-        """Convert `Schedule` into a pandas `DataFrame` (metadata field(s) are not included in the resultant `DataFrame`)"""
+    @property
+    def cn(self):
+        """A shortcut to return the DataFrameColumnNames class"""
 
-        df = pd.DataFrame({f.name: getattr(self, f.name) for f in Schedule.non_metadata_fields()})
-        df = df.set_index("stt_tstmp_us")
+        return DataFrameColumnNames
+
+    def as_dataframe(self) -> pd.DataFrame:
+        """
+        Convert `Schedule` into a pandas `DataFrame`.
+
+        Some extra derived columns are generated in the resultant `DataFrame`, while metadata field(s) are not included.
+
+        Handy for manipulation and plotting.
+        """
+
+        df = pd.DataFrame({c: getattr(self, c) for c in DataFrameColumnNames.all_non_derived()})
+
+        # add "end_time" column
+        df[self.cn.end_time] = df[self.cn.start_time] + np.array(
+            [self.meta[n].slice_duration for n in self.exp_num]
+        )
 
         return df
 
