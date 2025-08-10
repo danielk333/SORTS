474d776693916ce966b6c36412e62fd4cec4c6e1
tszhinh <tsz-hin.heung@irf.se>
2025-08-10 18:57:32 +0200

    added more modules to api docs and enabled type hints

--- scripts/gen_api_ref_pages.py
+++ scripts/gen_api_ref_pages.py
@@ -20,6 +20,8 @@ nav = Nav()
 
 # we only gen api docs for some specific modules for now
 target_mod_paths: list[Path] = [
+    root_mod_dpath / "types.py",
+    root_mod_dpath / "utils.py",
     root_mod_dpath / "controller_v2",
     root_mod_dpath / "schedule_v2.py",
     root_mod_dpath / "scheduler_v2",
