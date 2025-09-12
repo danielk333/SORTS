- added early return in 'get_indexer_per_measurement' - added new TODOs - corrected rx sch masking in 'FenceScanController' - corrected 'start_time' param in `Schedule` construction in module 'tracker_controller'

src/sorts/controller/fence_scan_controller.py
@@ -78,7 +78,7 @@ def generate_from_state(spec: Spec, state: State) -> Output:
         (state["tx_schedule_size"] + pointings_per_cycle - 1) // pointings_per_cycle,
     )[:, : state["tx_schedule_size"]]
 
-    # mask tx values by min_elevation requirement,
+    # mask tx values by min_elevation requirement
     tx_mask = pointing_funcs.create_mask_by_min_elevation(
         tx_pointing, spec["tx_station"].min_elevation
     )
@@ -137,10 +137,12 @@ def generate_from_state(spec: Spec, state: State) -> Output:
         )[:, :rx_schedule_size]
         rx_pointings_simult_num = np.arange(rx_schedule_size) % len(spec["scan_range"])
 
-        # mask rx values by min_elevation requirement,
-        rx_mask = pointing_funcs.create_mask_by_min_elevation(
+        # mask rx values by min_elevation requirement, and has a corresponding tx value
+        rx_mask_by_min_elevation = pointing_funcs.create_mask_by_min_elevation(
             rx_pointings_enu, rx_station.min_elevation
         )
+        rx_mask_by_tx_mask = np.isin(rx_slice_start_time, tx_slice_start_time_masked)
+        rx_mask = np.logical_and(rx_mask_by_min_elevation, rx_mask_by_tx_mask)
         rx_slice_start_time_masked = rx_slice_start_time[rx_mask]
         rx_pointing_masked = rx_pointings_enu[:, rx_mask]
         rx_pointings_simult_num_masked = rx_pointings_simult_num[rx_mask]

src/sorts/controller/tracker_controller.py
@@ -57,7 +57,7 @@ def generate_from_state(spec: Spec, state: State) -> Output:
     tx_pointings = tx_pointings[:, tx_el_in_range_mask]
 
     rxs_pointings: list[EnuCoordinates] = []
-    rx_el_in_range_masks: list[npt.NDArray[np.bool]] = []
+    rx_el_in_range_with_tx_masks: list[npt.NDArray[np.bool]] = []
     for rx_station in spec["rx_stations"]:
         rx_pointings: EnuCoordinates = rx_station.enu(state["spobj_states"][:3])
 
@@ -65,43 +65,48 @@ def generate_from_state(spec: Spec, state: State) -> Output:
             loc_zenith, rx_pointings, degrees=True
         )
         rx_el_in_range_mask = rx_pointings_zenith_ang <= 90.0 - rx_station.min_elevation
-        rx_pointings = rx_pointings[:, rx_el_in_range_mask]
 
-        rx_el_in_range_masks.append(rx_el_in_range_mask)
+        rx_el_in_range_with_tx_mask = np.logical_and(tx_el_in_range_mask, rx_el_in_range_mask)
+        rx_el_in_range_with_tx_masks.append(rx_el_in_range_with_tx_mask)
+
+        rx_pointings = rx_pointings[:, rx_el_in_range_with_tx_mask]
         rxs_pointings.append(rx_pointings)
 
-    el_in_range_mask = np.logical_and.reduce([tx_el_in_range_mask, *rx_el_in_range_masks])
-    sch_time = state["spobj_time"][el_in_range_mask]
-    sch_len = len(sch_time)
-
+    tx_sch_time = state["spobj_time"][tx_el_in_range_mask]
+    tx_sch_len = len(tx_sch_time)
     tx_sch = Schedule.from_ndarrays(
         data={
             "stn_id": spec["tx_station"].uid,
             "exp_detail_map": {spec["exp_detail"]["id"]: spec["exp_detail"]},
-            "start_time": sch_time,
-            "end_time": sch_time + spec["exp_detail"]["slice_duration"],
-            "exp_num": np.full(sch_len, spec["exp_detail"]["id"], dtype=np.int16),
-            "simult_num": np.full(sch_len, 0, dtype=np.int16),
+            "start_time": tx_sch_time,
+            "end_time": tx_sch_time + spec["exp_detail"]["slice_duration"],
+            "exp_num": np.full(tx_sch_len, spec["exp_detail"]["id"], dtype=np.int16),
+            "simult_num": np.full(tx_sch_len, 0, dtype=np.int16),
             "pointing": tx_pointings,
         },
         station=spec["tx_station"],
     )
 
-    rx_schs = [
-        Schedule.from_ndarrays(
-            data={
-                "stn_id": spec["rx_stations"][idx].uid,
-                "exp_detail_map": {spec["exp_detail"]["id"]: spec["exp_detail"]},
-                "start_time": sch_time,
-                "end_time": sch_time + spec["exp_detail"]["slice_duration"],
-                "exp_num": np.full(sch_len, spec["exp_detail"]["id"], dtype=np.int16),
-                "simult_num": np.full(sch_len, 0, dtype=np.int16),
-                "pointing": rx_pointings,
-            },
-            station=spec["rx_stations"][idx],
+    rx_schs: list[Schedule] = []
+    for rx_stn, rx_mask, rx_pointings in zip(
+        spec["rx_stations"], rx_el_in_range_with_tx_masks, rxs_pointings
+    ):
+        rx_sch_time = state["spobj_time"][rx_mask]
+        rx_sch_len = len(rx_sch_time)
+        rx_schs.append(
+            Schedule.from_ndarrays(
+                data={
+                    "stn_id": rx_stn.uid,
+                    "exp_detail_map": {spec["exp_detail"]["id"]: spec["exp_detail"]},
+                    "start_time": rx_sch_time,
+                    "end_time": rx_sch_time + spec["exp_detail"]["slice_duration"],
+                    "exp_num": np.full(rx_sch_len, spec["exp_detail"]["id"], dtype=np.int16),
+                    "simult_num": np.full(rx_sch_len, 0, dtype=np.int16),
+                    "pointing": rx_pointings,
+                },
+                station=rx_stn,
+            )
         )
-        for idx, rx_pointings in enumerate(rxs_pointings)
-    ]
 
     output = Output(tx_sch, rx_schs)
     return output