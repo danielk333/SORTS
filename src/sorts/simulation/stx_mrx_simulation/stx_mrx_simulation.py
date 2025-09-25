46acbc936d66502bee8f77fbfff63830376f8c53
tszhinh <tsz-hin.heung@irf.se>
2025-09-25 12:58:52 +0200

    extracted the mpi proc loops as individual funcs for clarity

--- src/sorts/simulation/stx_mrx_simulation/stx_mrx_simulation.py
+++ src/sorts/simulation/stx_mrx_simulation/stx_mrx_simulation.py
@@ -51,6 +51,158 @@ class Spec(t.TypedDict):
     interpolator_class: type[Interpolator]
 
 
+def mpi_master_proc_loop(
+    comm: MPI.Intracomm,
+    master_proc_rank: int,
+    spec: Spec,
+    rank_size: int,
+):
+    logger.debug(f"running in mpi with rank: {rank_size}")
+    logger.info(f"master: {master_proc_rank} | simulation preparation start")
+
+    sim_units = []
+    obss = []
+
+    spobjs_smpl_dsec, spobjs_smpl_states = funcs.sample_and_propagate_space_objects_states(
+        sampler=spec["dsec_sampler"],
+        spobjs=spec["space_objects"],
+        start_time=to_datetime64_us(spec["start_time"]),
+        end_time=to_datetime64_us(spec["end_time"]),
+    )
+    logger.debug("sample and propagate done")
+
+    spobjs_interpolators = [
+        spec["interpolator_class"](spobj_smpl_states, spobj_smpl_dsec)
+        for spobj_smpl_dsec, spobj_smpl_states in zip(spobjs_smpl_dsec, spobjs_smpl_states)
+    ]
+    logger.debug("interpolators done")
+
+    passages_lists = funcs.find_passages(
+        spec=spec,
+        spobjs_smpl_dsec=spobjs_smpl_dsec,
+        spobjs_smpl_states=spobjs_smpl_states,
+    )
+    logger.debug("find_passages done")
+
+    sim_units_param = funcs.derive_simulation_unit_params(
+        spec=spec,
+        passages_lists=passages_lists,
+        spobjs_interpolators=spobjs_interpolators,
+    )
+
+    logger.info(f"master: {master_proc_rank} | simulation preparation done")
+
+    ##
+    # parallization section
+    ##
+    logger.info(f"master: {master_proc_rank} | parallel processing of SimulationUnit start")
+
+    obss: list[Observation] = []
+
+    num_workers = comm.Get_size() - 1
+    is_worker_idle_list = [True for _ in range(num_workers)]
+    processed_sim_unit_cnt = 0
+    next_sim_unit_idx = 0
+
+    while processed_sim_unit_cnt < len(sim_units_param):
+
+        # TODO: would be more robust to check ids in sim_units_param than looping next_sim_unit_idx
+        # send sim_unit if there is idle worker
+        if any(is_worker_idle_list) and next_sim_unit_idx < len(sim_units_param):
+
+            idle_worker_idx = is_worker_idle_list.index(True)
+            idle_worker_rank = idle_worker_idx + 1
+            # TODO: check if the  (full ScheduleData + indexer for SimulationUnit) or (just the relevant slices of ScheduleData) are sent
+            comm.send(sim_units_param[next_sim_unit_idx], dest=idle_worker_rank)
+
+            logger.debug(
+                f"master: {master_proc_rank} | sent `SimulationUnit` {next_sim_unit_idx+1} of {len(sim_units_param)} to worker {idle_worker_rank}"
+            )
+
+            is_worker_idle_list[idle_worker_idx] = False
+            next_sim_unit_idx += 1
+
+        else:
+            # otherwise, wait for result
+            logger.debug(f"master: {master_proc_rank} | awaiting results ...")
+
+            status = MPI.Status()
+            recv_obss = comm.recv(status=status)
+            worker_rank = status.Get_source()
+            logger.debug(
+                f"master: {master_proc_rank} | received {len(recv_obss)} observations from worker: {worker_rank}"
+            )
+            processed_sim_unit_cnt += 1
+
+            is_worker_idle_list[worker_rank - 1] = True
+            obss.extend(recv_obss)
+
+    logger.info(f"master: {master_proc_rank} | parallel processing of SimulationUnit done")
+
+    # TODO: maybe we can use `comm.bcast` here?
+    #   but worker also need to call `comm.bcast` for listening,
+    #   not sure if mpi allows listening to both `bcast` and `recv`
+    for r in range(1, num_workers + 1):
+        logger.debug(f"master: {master_proc_rank} | terminating worker: {r} ...")
+        comm.send(_MpiMsg.terminate, dest=r)
+        comm.recv(source=r)  # wait for an ack
+
+    logger.info(f"master: {master_proc_rank} | `mpi_run` done,  returning...")
+    return obss, sim_units
+
+
+def mpi_worker_proc_loop(
+    comm: MPI.Intracomm, master_proc_rank: int, worker_proc_rank: int, persist_dir: Path
+):
+    while True:
+        logger.info(f"worker: {worker_proc_rank} | waiting for msg...")
+        msg = comm.recv(source=master_proc_rank)
+
+        # exit if `_MpiMsg.terminate` is received
+        if msg == _MpiMsg.terminate:
+            logger.info(f"worker: {worker_proc_rank} | exiting...")
+            comm.send(_MpiMsg.exit_ok, dest=master_proc_rank)  # reply an ack to master
+            exit()
+
+        # throw if the msg is not a `FromPassagesOverTxRxStationPairParam`
+        if not isinstance(msg, dict):
+            raise RuntimeError(f"worker: {worker_proc_rank} | received unexcepted msg: {msg}")
+
+        param = t.cast(FromPassagesOverTxRxStationPairParam, msg)
+
+        persist_fpath = persist_dir / f"{param["id"]}.pickle"
+        if persist_fpath.exists():
+            logger.info(
+                f"worker: {worker_proc_rank} | SimulationUnit: {param["id"]} already completed, will load from the saved file instead"
+            )
+
+            with open(persist_fpath, "rb") as f:
+                sim_unit = pickle.load(f)
+
+        else:
+            sim_unit = SimulationUnit.from_passages_over_tx_rx_station_pair(**param)
+
+            logger.info(f"worker: {worker_proc_rank} | `SimulationUnit.simulate` start")
+            sim_unit.simulate()
+
+            # As a simple way to reduce risk of corrupted files,
+            # we write to an tmp file first then rename that file
+            persist_fpath_tmp = persist_fpath.with_suffix(persist_fpath.suffix + ".tmp")
+            with open(persist_fpath_tmp, "wb") as f:
+                pickle.dump(sim_unit, f)
+                persist_fpath_tmp.rename(persist_fpath)
+
+        obss = funcs.derive_observations(
+            passages=param["passages"],
+            tx_schedule=param["tx_sch"],
+            rx_schedule=param["rx_sch"],
+            sim_unit=sim_unit,
+        )
+
+        comm.send(obss, dest=master_proc_rank)
+        logger.info(f"worker: {worker_proc_rank} | SimulationUnit:{sim_unit.id} done")
+
+
 # TODO: we need to enforce each station to has a unique id (`.uid` prop)
 #   either in the simulation class or in related station getter like `get_radar`
 class StxMrxSimulation:
@@ -129,145 +281,20 @@ class StxMrxSimulation:
         rank_size = comm.Get_size()
 
         if r == master_proc_rank:  # master
-            logger.debug(f"running in mpi with rank: {rank_size}")
-            logger.info(f"master: {master_proc_rank} | simulation preparation start")
-
-            self.sim_units = []
-            self.obss = []
-
-            spobjs_smpl_dsec, spobjs_smpl_states = funcs.sample_and_propagate_space_objects_states(
-                sampler=self.spec["dsec_sampler"],
-                spobjs=self.spec["space_objects"],
-                start_time=to_datetime64_us(self.spec["start_time"]),
-                end_time=to_datetime64_us(self.spec["end_time"]),
+            obss, sim_units = mpi_master_proc_loop(
+                comm=comm, master_proc_rank=r, spec=self.spec, rank_size=rank_size
             )
-            logger.debug("sample and propagate done")
-
-            spobjs_interpolators = [
-                self.spec["interpolator_class"](spobj_smpl_states, spobj_smpl_dsec)
-                for spobj_smpl_dsec, spobj_smpl_states in zip(spobjs_smpl_dsec, spobjs_smpl_states)
-            ]
-            logger.debug("interpolators done")
-
-            passages_lists = funcs.find_passages(
-                spec=self.spec,
-                spobjs_smpl_dsec=spobjs_smpl_dsec,
-                spobjs_smpl_states=spobjs_smpl_states,
-            )
-            logger.debug("find_passages done")
-
-            sim_units_param = funcs.derive_simulation_unit_params(
-                spec=self.spec,
-                passages_lists=passages_lists,
-                spobjs_interpolators=spobjs_interpolators,
-            )
-
-            logger.info(f"master: {master_proc_rank} | simulation preparation done")
-
-            ##
-            # parallization section
-            ##
-            logger.info(f"master: {master_proc_rank} | parallel processing of SimulationUnit start")
-
-            obss: list[Observation] = []
-
-            num_workers = comm.Get_size() - 1
-            is_worker_idle_list = [True for _ in range(num_workers)]
-            processed_sim_unit_cnt = 0
-            next_sim_unit_idx = 0
-
-            while processed_sim_unit_cnt < len(sim_units_param):
-
-                # TODO: would be more robust to check ids in sim_units_param than looping next_sim_unit_idx
-                # send sim_unit if there is idle worker
-                if any(is_worker_idle_list) and next_sim_unit_idx < len(sim_units_param):
-
-                    idle_worker_idx = is_worker_idle_list.index(True)
-                    idle_worker_rank = idle_worker_idx + 1
-                    # TODO: check if the  (full ScheduleData + indexer for SimulationUnit) or (just the relevant slices of ScheduleData) are sent
-                    comm.send(sim_units_param[next_sim_unit_idx], dest=idle_worker_rank)
-
-                    logger.debug(
-                        f"master: {master_proc_rank} | sent `SimulationUnit` {next_sim_unit_idx+1} of {len(sim_units_param)} to worker {idle_worker_rank}"
-                    )
-
-                    is_worker_idle_list[idle_worker_idx] = False
-                    next_sim_unit_idx += 1
-
-                else:
-                    # otherwise, wait for result
-                    logger.debug(f"master: {master_proc_rank} | awaiting results ...")
-
-                    status = MPI.Status()
-                    recv_obss = comm.recv(status=status)
-                    worker_rank = status.Get_source()
-                    logger.debug(
-                        f"master: {master_proc_rank} | received {len(recv_obss)} observations from worker: {worker_rank}"
-                    )
-                    processed_sim_unit_cnt += 1
-
-                    is_worker_idle_list[worker_rank - 1] = True
-                    obss.extend(recv_obss)
 
             self.obss = obss
-            logger.info(f"master: {master_proc_rank} | parallel processing of SimulationUnit done")
-
-            # TODO: maybe we can use `comm.bcast` here?
-            #   but worker also need to call `comm.bcast` for listening,
-            #   not sure if mpi allows listening to both `bcast` and `recv`
-            for r in range(1, num_workers + 1):
-                logger.debug(f"master: {master_proc_rank} | terminating worker: {r} ...")
-                comm.send(_MpiMsg.terminate, dest=r)
-                comm.recv(source=r)  # wait for an ack
-
-            logger.info(f"master: {master_proc_rank} | `mpi_run` done,  returning...")
+            self.sim_units = sim_units
             return self.obss, self.sim_units
 
         else:  # workers
-            while True:
-                logger.info(f"worker: {r} | waiting for msg...")
-                msg = comm.recv(source=master_proc_rank)
+            mpi_worker_proc_loop(
+                comm=comm,
+                master_proc_rank=master_proc_rank,
+                worker_proc_rank=r,
+                persist_dir=persist_dir,
+            )
 
-                # exit if `_MpiMsg.terminate` is received
-                if msg == _MpiMsg.terminate:
-                    logger.info(f"worker: {r} | exiting...")
-                    comm.send(_MpiMsg.exit_ok, dest=master_proc_rank)  # reply an ack to master
-                    exit()
-
-                # throw if the msg is not a `FromPassagesOverTxRxStationPairParam`
-                if not isinstance(msg, dict):
-                    raise RuntimeError(f"worker: {r} | received unexcepted msg: {msg}")
-
-                param = t.cast(FromPassagesOverTxRxStationPairParam, msg)
-
-                persist_fpath = persist_dir / f"{param["id"]}.pickle"
-                if persist_fpath.exists():
-                    logger.info(
-                        f"worker: {r} | SimulationUnit: {param["id"]} already completed, will load from the saved file instead"
-                    )
-
-                    with open(persist_fpath, "rb") as f:
-                        sim_unit = pickle.load(f)
-
-                else:
-                    sim_unit = SimulationUnit.from_passages_over_tx_rx_station_pair(**param)
-
-                    logger.info(f"worker: {r} | `SimulationUnit.simulate` start")
-                    sim_unit.simulate()
-
-                    # As a simple way to reduce risk of corrupted files,
-                    # we write to an tmp file first then rename that file
-                    persist_fpath_tmp = persist_fpath.with_suffix(persist_fpath.suffix + ".tmp")
-                    with open(persist_fpath_tmp, "wb") as f:
-                        pickle.dump(sim_unit, f)
-                        persist_fpath_tmp.rename(persist_fpath)
-
-                obss = funcs.derive_observations(
-                    passages=param["passages"],
-                    tx_schedule=param["tx_sch"],
-                    rx_schedule=param["rx_sch"],
-                    sim_unit=sim_unit,
-                )
-
-                comm.send(obss, dest=master_proc_rank)
-                logger.info(f"worker: {r} | SimulationUnit:{sim_unit.id} done")
+            return self.obss, self.sim_units
