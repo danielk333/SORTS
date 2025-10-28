cd7ac22874bb2eb24819b4d6fa29ff5aa7f4b8a3
tszhinh <tsz-hin.heung@irf.se>
2025-10-28 15:35:51 +0100

    - merged func 'generate_from_state' into method 'generate' - adj its signature and removed unnneded checks

--- src/sorts/controller/sparse_tracker_controller.py
+++ src/sorts/controller/sparse_tracker_controller.py
@@ -40,77 +40,6 @@ class ControllerState:
     spobj_states: EcefStates
 
 
-def generate_from_state(spec: ControllerSpec, state: ControllerState) -> schedule.Schedule:
-    passages_of_spobj = simulation.find_simultaneous_passages(
-        dt=(state.spobj_time - spec.epoch) / np.timedelta64(1, "s"),
-        space_object=spec.space_object,
-        states=state.spobj_states[:3, ...],
-        tx_station=spec.tx_station,
-        rx_stations=spec.rx_stations,
-        epoch=spec.epoch,
-    )
-
-    tx_sch_index_list = []
-    for ps in passages_of_spobj:
-        start_time, end_time = ps["time_range"]
-        passage_time = (end_time - start_time) / np.timedelta64(1, "s")
-        relative_time_sampling = np.linspace(
-            0.0, passage_time, num=spec.points_per_passage + 2, endpoint=True
-        )
-        relative_time_sampling = relative_time_sampling[1:-1]
-        # TODO: once the propagator sampling has been changed, use a interpolator here instead
-        # at the cadence that the propagator currently uses
-        pass_tx_index = np.empty((spec.points_per_passage,), dtype=np.int64)
-        for ind in range(spec.points_per_passage):
-            pass_tx_index[ind] = np.argmin(
-                np.abs(
-                    (relative_time_sampling[ind] + start_time - state.spobj_time)
-                    / np.timedelta64(1, "s")
-                )
-            )
-        tx_sch_index_list.append(pass_tx_index)
-    tx_sch_index = np.concatenate(tx_sch_index_list)
-    tx_sch_time = state.spobj_time[tx_sch_index]
-    tx_sch_len = len(tx_sch_time)
-    tx_pointings: EnuCoordinates = spec.tx_station.enu(state.spobj_states[:3, tx_sch_index])
-    tx_pointings = tx_pointings / np.linalg.norm(tx_pointings, axis=0)
-
-    tx_sch = schedule.from_ndarrays(
-        {
-            "start_time": tx_sch_time,
-            "end_time": tx_sch_time + spec.exp_detail["slice_duration"],
-            "exp_num": np.full(tx_sch_len, spec.exp_detail["id"], dtype=np.int16),
-            "stn_num": np.full(tx_sch_len, spec.tx_station.uid, dtype=np.int16),
-            "simult_num": np.full(tx_sch_len, 0, dtype=np.int16),
-            "pointing": tx_pointings,
-        }
-    )
-
-    rx_schs: list[schedule.Schedule] = []
-    for rx_stn in spec.rx_stations:
-        rx_pointings: EnuCoordinates = rx_stn.enu(state.spobj_states[:3, tx_sch_index])
-        rx_pointings = rx_pointings / np.linalg.norm(rx_pointings, axis=0)
-
-        rx_schs.append(
-            schedule.from_ndarrays(
-                {
-                    "start_time": tx_sch_time,
-                    "end_time": tx_sch_time + spec.exp_detail["slice_duration"],
-                    "exp_num": np.full(tx_sch_len, spec.exp_detail["id"], dtype=np.int16),
-                    "stn_num": np.full(tx_sch_len, rx_stn.uid, dtype=np.int16),
-                    "simult_num": np.full(tx_sch_len, 0, dtype=np.int16),
-                    "pointing": rx_pointings,
-                }
-            )
-        )
-
-    resultant_sch = xr.concat([tx_sch, *rx_schs], dim=schedule._K.multi_index)
-    resultant_sch = resultant_sch.sortby(schedule._K.start_time)
-    output = resultant_sch
-
-    return output
-
-
 class SparseTrackerController(ControllerBase):
     """
     Generate pointing schedule that tracks a space object.
@@ -203,26 +132,80 @@ class SparseTrackerController(ControllerBase):
 
         return self
 
-    def generate(
-        self, start_time: Datetime_Like | None = None, end_time: Datetime_Like | None = None
-    ) -> schedule.Schedule:
+    def generate(self, start_time: Datetime_Like, end_time: Datetime_Like) -> schedule.Schedule:
         """
         Generate the schedules.
         `start_time` and `end_time` should be omitted if this instance is created from `TrackerController.from_ecef_states`
         """
 
-        if start_time is not None and end_time is not None:
-            self.compute_ecef_states(start_time, end_time, self.exp_detail["slice_duration"])
-            state = t.cast(ControllerState, self.state)
-        elif self.state is None:
-            raise RuntimeError(
-                "Cannot generate without valid state property."
-                + " Please either provide the `start_time` and `end_time` param"
-                + " or ensure it is set correctly using methods like `compute_ecef_states` or proper constructors."
-            )
-        else:
-            state = self.state
+        self.compute_ecef_states(start_time, end_time, self.exp_detail["slice_duration"])
+        state = t.cast(ControllerState, self.state)
 
-        output = generate_from_state(spec=self.spec, state=state)
+        passages_of_spobj = simulation.find_simultaneous_passages(
+            dt=(state.spobj_time - self.epoch) / np.timedelta64(1, "s"),
+            space_object=self.space_object,
+            states=state.spobj_states[:3, ...],
+            tx_station=self.tx_station,
+            rx_stations=self.rx_stations,
+            epoch=self.epoch,
+        )
+
+        tx_sch_index_list = []
+        for ps in passages_of_spobj:
+            start_time, end_time = ps["time_range"]
+            passage_time = (end_time - start_time) / np.timedelta64(1, "s")
+            relative_time_sampling = np.linspace(
+                0.0, passage_time, num=self.points_per_passage + 2, endpoint=True
+            )
+            relative_time_sampling = relative_time_sampling[1:-1]
+            # TODO: once the propagator sampling has been changed, use a interpolator here instead
+            # at the cadence that the propagator currently uses
+            pass_tx_index = np.empty((self.points_per_passage,), dtype=np.int64)
+            for ind in range(self.points_per_passage):
+                pass_tx_index[ind] = np.argmin(
+                    np.abs(
+                        (relative_time_sampling[ind] + start_time - state.spobj_time)
+                        / np.timedelta64(1, "s")
+                    )
+                )
+            tx_sch_index_list.append(pass_tx_index)
+        tx_sch_index = np.concatenate(tx_sch_index_list)
+        tx_sch_time = state.spobj_time[tx_sch_index]
+        tx_sch_len = len(tx_sch_time)
+        tx_pointings: EnuCoordinates = self.tx_station.enu(state.spobj_states[:3, tx_sch_index])
+        tx_pointings = tx_pointings / np.linalg.norm(tx_pointings, axis=0)
+
+        tx_sch = schedule.from_ndarrays(
+            {
+                "start_time": tx_sch_time,
+                "end_time": tx_sch_time + self.exp_detail["slice_duration"],
+                "exp_num": np.full(tx_sch_len, self.exp_detail["id"], dtype=np.int16),
+                "stn_num": np.full(tx_sch_len, self.tx_station.uid, dtype=np.int16),
+                "simult_num": np.full(tx_sch_len, 0, dtype=np.int16),
+                "pointing": tx_pointings,
+            }
+        )
+
+        rx_schs: list[schedule.Schedule] = []
+        for rx_stn in self.rx_stations:
+            rx_pointings: EnuCoordinates = rx_stn.enu(state.spobj_states[:3, tx_sch_index])
+            rx_pointings = rx_pointings / np.linalg.norm(rx_pointings, axis=0)
+
+            rx_schs.append(
+                schedule.from_ndarrays(
+                    {
+                        "start_time": tx_sch_time,
+                        "end_time": tx_sch_time + self.exp_detail["slice_duration"],
+                        "exp_num": np.full(tx_sch_len, self.exp_detail["id"], dtype=np.int16),
+                        "stn_num": np.full(tx_sch_len, rx_stn.uid, dtype=np.int16),
+                        "simult_num": np.full(tx_sch_len, 0, dtype=np.int16),
+                        "pointing": rx_pointings,
+                    }
+                )
+            )
+
+        resultant_sch = xr.concat([tx_sch, *rx_schs], dim=schedule._K.multi_index)
+        resultant_sch = resultant_sch.sortby(schedule._K.start_time)
+        output = resultant_sch
 
         return output
