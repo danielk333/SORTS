b85fb3a764bdc470fc913713d11d51e556191b02
Daniel Kastinen <daniel.kastinen@irf.se>
2025-10-28 12:20:50 +0100

    added simult passage function

--- src/sorts/simulation/__init__.py
+++ src/sorts/simulation/__init__.py
@@ -3,3 +3,4 @@ from . import types, funcs, stx_mrx_simulation
 
 # TODO: remove `Spec` from here; at the use site, import it from the re-exported `stx_mrx_simulation` module instead
 from .stx_mrx_simulation import Spec, StxMrxSimulation
+from .funcs import find_passages, find_simultaneous_passages
