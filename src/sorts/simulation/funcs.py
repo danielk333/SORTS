bbb68f8af04f8be85107cc23e91e927a3b94f257
Daniel Kastinen <daniel.kastinen@irf.se>
2025-12-09 16:47:25 +0100

    added a container for propagations and interpolations - one of the most common patterns

--- src/sorts/simulation/funcs.py
+++ src/sorts/simulation/funcs.py
@@ -6,15 +6,54 @@ Functions for core functionalities of this subpackage
 
 # TODO: this module can be moved to top level as helper funcs of sorts pkg?
 
-import typing as t, pickle
+from dataclasses import dataclass
+import typing as t
+import pickle
 from pathlib import Path
 import numpy as np
 import numpy.typing as npt
+from astropy.time import Time
 from sorts.types import Datetime64_us, EcefStates, Float64_as_sec, Datetime_Like
 from sorts.utils import to_datetime64_us
 from sorts.radar import Station
 from sorts.space_object import SpaceObject
-from .types import SimultaneousPassage, Passage, SpaceObjectJacobianTuple
+from .types import Passage, SpaceObjectJacobianTuple
+from sorts.interpolation import Interpolator
+from sorts.propagator import Propagator
+from sorts.population import Population
+
+
+@dataclass
+class InterpolatedPropagation:
+    times: npt.NDArray[Datetime64_us]
+    states: EcefStates
+    interpolator: Interpolator
+
+    @classmethod
+    def from_space_objects(
+        cls,
+        space_objects: t.Sequence[SpaceObject] | Population,
+        propagator: Propagator,
+        interpolator_class: t.Type[Interpolator],
+        start_time: Time,
+        end_time: Time,
+        time_step: float,
+    ) -> list[t.Self]:
+        dt = (end_time - start_time).sec
+        prop_interps = []
+        for spobj in space_objects:
+            t0 = (start_time - spobj.epoch).sec
+            t_obj = np.arange(t0, t0 + dt, time_step, dtype=np.float64)
+
+            itrs_states = propagator.propagate(spobj, t_obj)
+            interp = interpolator_class(itrs_states, t_obj)
+            pint = cls(
+                times=spobj.epoch.datetime64 + t_obj,
+                states=itrs_states,
+                interpolator=interp,
+            )
+            prop_interps.append(pint)
+        return prop_interps
 
 
 def find_simultaneous_passages(
@@ -96,7 +135,6 @@ def find_passages(
     """
     Finds all passages that are simultaneously inside a tx-rx station pair's FOV.
     """
-
     passages = find_simultaneous_passages(
         dt=dt,
         space_object=space_object,
@@ -111,10 +149,15 @@ def find_passages(
 
 def duplicate_and_perturbate_space_objects(
     spobjs: t.Sequence[SpaceObject],
+    propagator: Propagator,
+    interpolator_class: t.Type[Interpolator],
+    start_time: Time,
+    end_time: Time,
+    time_step: float,
     pert_val: tuple[float, float, float, float, float, float] = (
         1e-3, 1e-3, 1e-3, 1e-5, 1e-5, 1e-5  # fmt: skip
     ),
-) -> list[SpaceObjectJacobianTuple]:
+) -> list[tuple[SpaceObjectJacobianTuple, InterpolatedPropagation]]:
     # duplicate list items
     spobjs_jacobian_tuples = [
         (spobj, spobj.copy(), spobj.copy(), spobj.copy(), spobj.copy(), spobj.copy(), spobj.copy())
@@ -128,7 +171,15 @@ def duplicate_and_perturbate_space_objects(
             if idx != 0:
                 spobj.state._cart[idx - 1, 0] += pert_val[idx - 1]
 
-    return spobjs_jacobian_tuples
+    prop_interps = InterpolatedPropagation.from_space_objects(
+        space_objects=spobjs,
+        propagator=propagator,
+        interpolator_class=interpolator_class,
+        start_time=start_time,
+        end_time=end_time,
+        time_step=time_step,
+    )
+    return list(zip(spobjs_jacobian_tuples, prop_interps))
 
 
 def ensure_directory_exist(dpath: str | Path):
