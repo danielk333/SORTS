0286f1db9a1e61afd346ec0ee68e2c626aa14e3b
tszhinh <tsz-hin.heung@irf.se>
2026-02-04 16:16:27 +0100

    renamed example 'sparse_tracking_exp_trial_01.py' to 'sparse_tracking_simulation_with_mpi'

--- examples/v2/sparse_tracking_simulation_with_mpi.py
+++ examples/v2/sparse_tracking_simulation_with_mpi.py
@@ -23,7 +23,7 @@ logger.info("starting example")
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--out_dir", type=Path, default=Path(__file__).parent.parent / "data")
-parser.add_argument("--name", type=str, default="sparse_tracking_nostra_sim0")
+parser.add_argument("--name", type=str, default="sparse_tracking_simulation_with_mpi")
 parser.add_argument("--clobber", action="store_true")
 parser.add_argument("--antennas", type=int, default=10_000)
 args = parser.parse_args()
