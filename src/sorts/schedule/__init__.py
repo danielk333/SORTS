2ad8d25b7ede692af5ebca127d14df273342c4e1
tszhinh <tsz-hin.heung@irf.se>
2026-02-23 13:18:20 +0100

    reorganize schedule submodules

--- src/sorts/schedule/__init__.py
+++ src/sorts/schedule/__init__.py
@@ -1,16 +1,23 @@
-from .schedule import (
+from . import types, schedule, schedule_dataframe
+
+from .types import (
     ScheduleKey,
-    ScheduleDataframe,
-    ScheduleDbConnection,
-    ScheduleDb,
-    validate_schedule_dataframe,
-    schedule_dataframe_from_rows,
-    schedule_dataframe_from_series,
-    schedule_dataframe_from_ndarrays,
-    empty_schedule_dataframe,
     ExperimentId,
     SimultaneousNum,
     ExperimentDetail,
     ExperimentDetailMap,
     ExperimentIdStationIdPairsMap,
+)
+from .schedule import (
+    ScheduleDbConnection,
+    ScheduleDb,
+)
+from .schedule_dataframe import (
+    ScheduleDataframe,
+    scheduleDataframeDtypes,
+    validate_schedule_dataframe,
+    schedule_dataframe_from_rows,
+    schedule_dataframe_from_series,
+    schedule_dataframe_from_ndarrays,
+    empty_schedule_dataframe,
 )
---
+++ src/sorts/schedule/schedule_dataframe.py
@@ -0,0 +1,175 @@
+from __future__ import annotations
+import logging, typing as t
+import numpy as np
+import numpy.typing as npt
+import pandas as pd
+import pandas._typing as pdt
+from .types import ScheduleKey, ScheduleValidationError
+
+logger = logging.getLogger(__name__)
+
+ScheduleDataframe = t.NewType("ScheduleDataframe", pd.DataFrame)
+"""
+A pandas `DataFrame` which:
+- Has an index without name or named as `"index"`
+- Contains all the following columns
+    ```
+    - "exp_num":     np.int16
+    - "stn_num":     np.int16
+    - "simult_num":  np.int16
+    - "start_time":  "datetime64[us]"
+    - "end_time":    "datetime64[us]"
+    - "pointing_e":  np.float64
+    - "pointing_n":  np.float64
+    - "pointing_u":  np.float64
+    ```
+
+The keys are available as enum `ScheduleKey` for consistent access.
+
+NOTE: This is intended as an internal constructor, please use the constructor functions to create instances.
+"""
+
+scheduleDataframeDtypes: t.Final[dict[t.Hashable, pdt.Dtype]] = {
+    ScheduleKey.exp_num: "int16",
+    ScheduleKey.stn_num: "int16",
+    ScheduleKey.simult_num: "int16",
+    ScheduleKey.start_time: "datetime64[us]",
+    ScheduleKey.end_time: "datetime64[us]",
+    ScheduleKey.pointing_e: "float64",
+    ScheduleKey.pointing_n: "float64",
+    ScheduleKey.pointing_u: "float64",
+}
+"""
+The dtypes of a `ScheduleDataframe` expressed in a python dict.
+Useful for certain pandas IO methods.
+"""
+
+
+def validate_schedule_dataframe(df: pd.DataFrame) -> ScheduleDataframe:
+    """
+    Validate a pandas `DataFrame` against the definition of `ScheduleDataframe`.
+
+    See the docs of `ScheduleDataframe` for its definition.
+
+    Returns:
+        The original DataFrame casted into a `ScheduleDataframe`.
+
+    Raises:
+        `ScheduleValidationError`
+    """
+
+    if not (df.index.name is None or df.index.name == ScheduleKey.index):
+        raise ScheduleValidationError("DataFrame index name is not `None` or `'index'`")
+
+    if not {key.value for key in ScheduleKey if key != ScheduleKey.index}.issubset(df.columns):
+        raise ScheduleValidationError("One or more column is missing from the DataFrame")
+
+    if df.dtypes[ScheduleKey.exp_num] != "int16":
+        raise ScheduleValidationError("Column 'exp_num' is not numpy dtype 'int16'")
+    if df.dtypes[ScheduleKey.stn_num] != "int16":
+        raise ScheduleValidationError("Column 'stn_num' is not numpy dtype 'int16'")
+    if df.dtypes[ScheduleKey.simult_num] != "int16":
+        raise ScheduleValidationError("Column 'simult_num' is not numpy dtype 'int16'")
+
+    if df.dtypes[ScheduleKey.start_time] != "datetime64[us]":
+        raise ScheduleValidationError("Column 'start_time' is not numpy dtype 'datetime64[us]'")
+    if df.dtypes[ScheduleKey.end_time] != "datetime64[us]":
+        raise ScheduleValidationError("Column 'end_time' is not numpy dtype 'datetime64[us]'")
+
+    if df.dtypes[ScheduleKey.pointing_e] != "float64":
+        raise ScheduleValidationError("Column 'pointing_e' is not numpy dtype 'float64'")
+    if df.dtypes[ScheduleKey.pointing_n] != "float64":
+        raise ScheduleValidationError("Column 'pointing_n' is not numpy dtype 'float64'")
+    if df.dtypes[ScheduleKey.pointing_u] != "float64":
+        raise ScheduleValidationError("Column 'pointing_u' is not numpy dtype 'float64'")
+
+    return ScheduleDataframe(df)
+
+
+def schedule_dataframe_from_rows(rows: list[list[t.Any]]) -> ScheduleDataframe:
+    """Create a `ScheduleDataframe` from rows of data."""
+
+    # NOTE: Constructing `DataFrame` from `Series` seems to be the only safe way to ensure
+    #       the datetime resolution is not overrided into `'ns'` from pandas's type infer attempt.
+
+    if len(rows) == 0:
+        cols = [[] for key in ScheduleKey if key != ScheduleKey.index]
+    else:
+        cols = list(zip(*rows))
+
+    df = pd.DataFrame(
+        {
+            ScheduleKey.exp_num: pd.Series(cols[0], dtype=np.int16),
+            ScheduleKey.stn_num: pd.Series(cols[1], dtype=np.int16),
+            ScheduleKey.simult_num: pd.Series(cols[2], dtype=np.int16),
+            ScheduleKey.start_time: pd.Series(cols[3], dtype="datetime64[us]"),
+            ScheduleKey.end_time: pd.Series(cols[4], dtype="datetime64[us]"),
+            ScheduleKey.pointing_e: pd.Series(cols[5], dtype=np.float64),
+            ScheduleKey.pointing_n: pd.Series(cols[6], dtype=np.float64),
+            ScheduleKey.pointing_u: pd.Series(cols[7], dtype=np.float64),
+        }
+    )
+
+    return validate_schedule_dataframe(df)
+
+
+def schedule_dataframe_from_series(
+    exp_num: pd.Series,
+    stn_num: pd.Series,
+    simult_num: pd.Series,
+    start_time: pd.Series,
+    end_time: pd.Series,
+    pointing_e: pd.Series,
+    pointing_n: pd.Series,
+    pointing_u: pd.Series,
+) -> ScheduleDataframe:
+    """Create an `ScheduleDataframe` from columns of pandas `Series`."""
+
+    df = pd.DataFrame(
+        {
+            ScheduleKey.exp_num: exp_num,
+            ScheduleKey.stn_num: stn_num,
+            ScheduleKey.simult_num: simult_num,
+            ScheduleKey.start_time: start_time,
+            ScheduleKey.end_time: end_time,
+            ScheduleKey.pointing_e: pointing_e,
+            ScheduleKey.pointing_n: pointing_n,
+            ScheduleKey.pointing_u: pointing_u,
+        }
+    )
+
+    return validate_schedule_dataframe(df)
+
+
+def schedule_dataframe_from_ndarrays(
+    exp_num: npt.NDArray,
+    stn_num: npt.NDArray,
+    simult_num: npt.NDArray,
+    start_time: npt.NDArray,
+    end_time: npt.NDArray,
+    pointing_e: npt.NDArray,
+    pointing_n: npt.NDArray,
+    pointing_u: npt.NDArray,
+) -> ScheduleDataframe:
+    """Create an empty `ScheduleDataframe` from columns of numpy `ndarray`."""
+
+    df = pd.DataFrame(
+        {
+            ScheduleKey.exp_num: pd.Series(exp_num, dtype=np.int16),
+            ScheduleKey.stn_num: pd.Series(stn_num, dtype=np.int16),
+            ScheduleKey.simult_num: pd.Series(simult_num, dtype=np.int16),
+            ScheduleKey.start_time: pd.Series(start_time, dtype="datetime64[us]"),
+            ScheduleKey.end_time: pd.Series(end_time, dtype="datetime64[us]"),
+            ScheduleKey.pointing_e: pd.Series(pointing_e, dtype=np.float64),
+            ScheduleKey.pointing_n: pd.Series(pointing_n, dtype=np.float64),
+            ScheduleKey.pointing_u: pd.Series(pointing_u, dtype=np.float64),
+        }
+    )
+
+    return validate_schedule_dataframe(df)
+
+
+def empty_schedule_dataframe() -> ScheduleDataframe:
+    """Create an empty `ScheduleDataframe`"""
+
+    return schedule_dataframe_from_rows([])
