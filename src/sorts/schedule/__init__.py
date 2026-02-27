b56f292eb9a0d8255eac737456c578cdd3f9e56b
tszhinh <tsz-hin.heung@irf.se>
2026-02-27 14:02:44 +0100

    import line correction

--- src/sorts/schedule/__init__.py
+++ src/sorts/schedule/__init__.py
@@ -1,4 +1,4 @@
-from . import types, schedule_db, schedule_dataframe
+from . import schedule_db, schedule_dataframe
 
 from .schedule_db import ScheduleDbConnection, ScheduleDb
 from .schedule_dataframe import ScheduleDataframe, scheduleDataframeDtypes
