5fa20eab71f84cab703857ac7118d23e03d88de8
tszhinh <tsz-hin.heung@irf.se>
2026-03-19 11:31:57 +0100

    improved typing and docs of func duplicate_and_perturbate_space_object

--- src/sorts/perturbation.py
+++ src/sorts/perturbation.py
@@ -1,6 +1,6 @@
 import typing as t
 from astropy.time import Time
-from sorts.types import StateType
+from sorts import types
 from sorts.space_object import SpaceObject
 from sorts.interpolation import Interpolator
 from sorts.propagator import Propagator
@@ -19,18 +19,27 @@ def duplicate_and_perturbate_space_object(
     start_time: Time,
     end_time: Time,
     time_step: float,
-    perturbation_format: StateType = "cartesian",
+    perturbation_format: types.StateType = "cartesian",
     pert_val: tuple[float, float, float, float, float, float] = (
         1e-3, 1e-3, 1e-3, 1e-5, 1e-5, 1e-5  # fmt: skip
     ),
-) -> list[SpaceObjectInterpolatedPropagationPair]:
-    # TODO: detail structure/explanation in docstring
+) -> types.Tuple_7[SpaceObjectInterpolatedPropagationPair]:
+    """
+    Perturbate the input space object by the specified method and values.
+
+    Returns:
+        A tuple of 7 `SpaceObjectInterpolatedPropagationPair`,
+        where the first one is for the true space object, and the reset follows the order of input perturbation value order:
+        ```
+        [(true_spobj, true_prop), (pert_spobj, pert_spobj_prop) ...x6]
+        ```
+    """
 
     # duplicate list items
-    perturbed_objects: list[SpaceObjectInterpolatedPropagationPair] = []
+    spobj_interp_prop_pairs: list[SpaceObjectInterpolatedPropagationPair] = []
 
     # perturbate all state variables and leave one original
-    # i.e. len 7, [(true_spobj_list, true_prop list), (pert_spobj_prop_list, ...) ...x6]
+    # i.e. len 7, [(true_spobj, true_prop), (pert_spobj, pert_spobj_prop) ...x6]
     for idx in range(7):
         # the original spobj are left intact, the rest are copied and perturbed
         if idx == 0:
@@ -53,10 +62,19 @@ def duplicate_and_perturbate_space_object(
             end_time=end_time,
             time_step=time_step,
         )
-        perturbed_objects.append(
+        spobj_interp_prop_pairs.append(
             SpaceObjectInterpolatedPropagationPair(
                 new_obj,
                 prop_interp,
             )
         )
-    return perturbed_objects
+
+    return (
+        spobj_interp_prop_pairs[0],
+        spobj_interp_prop_pairs[1],
+        spobj_interp_prop_pairs[2],
+        spobj_interp_prop_pairs[3],
+        spobj_interp_prop_pairs[4],
+        spobj_interp_prop_pairs[5],
+        spobj_interp_prop_pairs[6],
+    )
