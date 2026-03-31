27ddcaaf2fa38066af4c76ee0d5d8b3ce2fb9248
tszhinh <tsz-hin.heung@irf.se>
2026-03-31 16:12:46 +0200

    rewritten about half of test_south_to_north_circular_orbit

--- tests/integration/test_fence_scan.py
+++ tests/integration/test_fence_scan.py
@@ -1,10 +1,10 @@
 """
 We check against an imaginary circular orbit which
-- co-rotate with Earth
+- co-rotate with Earth (i.e. a circular orbit on a earth fixed coordinate system)
 - at 90deg inclination, 0 deg longitude
 - simulation start from the orbit's intersection with earth's equatorial plane, passing south hemisphere then north hemisphere
 
-we use 2 rx station and 2 scan ranges in this setup:
+we use 2 rx stations and 2 scan ranges in this setup:
 
 for scan ranges:
 - 1st range is a unrealistically low value for triggering masking effect due to min_elevation of station
@@ -19,25 +19,27 @@ for rx stations:
 
 import logging
 import numpy as np
+import numpy.typing as npt
+import pandas as pd
 from astropy.time import Time
 from astropy.constants import R_earth  # type: ignore
 import pyant, pyorb
 from sorts.types import Float64_as_sec, Float64_as_deg, Float_as_sec, Float_as_m
-from sorts.utils import to_datetime64_us
 from sorts.frames import enu_to_ecef
 from sorts import (
     types,
+    utils,
     schedule,
     pointing,
     interpolation,
     simulation,
     radar,
     propagator,
-    controller,
+    passage,
     SpaceObject,
     InterpolatedPropagation,
 )
-from sorts.simulation import stx_mrx_simulation
+from sorts.simulation import stx_mrx_simulation, tx_rx_pair_state
 
 
 logging.basicConfig(level=logging.INFO)
@@ -51,28 +53,31 @@ def setup_function():
     # https://github.com/pytest-dev/pytest/issues/8574#issuecomment-1806404215
     print()
 
+    import pandas as pd
 
-earth_radius: np.float64 = R_earth.value  # in meters
-spobj_orbital_radius = 7000e3  # in meters
+    pd.set_option("display.expand_frame_repr", False)
 
-float_equality_thld = 1e-9
-# TODO: re-eval this threshold
-pointing_range_equality_thld: Float_as_m = 1e-6
 
-# TODO: re-eval the `control_slice_duration` value, need to be fast but still accurate enough for testing
-# control_slice_duration = np.timedelta64(10_000, "us")  # 10ms
-control_slice_duration = np.timedelta64(1_000_000, "us")  # 1s
+# float_equality_thld = 1e-9
+# # TODO: re-eval this threshold
+# pointing_range_equality_thld: Float_as_m = 1e-6
 
-dt_equality_thld = control_slice_duration
-dsec_sampling_intv: Float_as_sec = 30
-scan_ranges = np.array([10, spobj_orbital_radius], dtype=np.float64)
-simu_num = len(scan_ranges)
+# dt_equality_thld = control_slice_duration
+# dsec_sampling_intv: Float_as_sec = 30
+# scan_ranges = np.array([10, spobj_orbital_radius], dtype=np.float64)
+# simu_num = len(scan_ranges)
 
-_SK = schedule.ScheduleKey
-_SuK = simulation.TxRxPairStateKey
+# _SK = schedule.ScheduleKey
+# _SuK = simulation.TxRxPairStateKey
 
 
 def test_south_to_north_circular_orbit():
+    earth_radius: np.float64 = R_earth.value  # in meters
+    spobj_orbital_radius = 7000e3  # in meters
+    scan_ranges = np.array([10, spobj_orbital_radius], dtype=np.float64)
+    num_prop_steps = 60
+    float_equality_thld = 1e-9
+
     spobj_orbital_period: Float64_as_sec = pyorb.orbital_period(
         spobj_orbital_radius, pyorb.GM_earth
     )
@@ -83,13 +88,14 @@ def test_south_to_north_circular_orbit():
     passage_duration: Float64_as_sec = passage_angular_duration / 360 * spobj_orbital_period
 
     start_time = Time("2025-01-01T02:45:00", format="isot", scale="utc")  # simulation start time
-    expected_passage_start_time = to_datetime64_us(start_time) + (
+    start_time_dt64 = utils.to_datetime64_us(start_time)
+    expected_passage_start_time = start_time_dt64 + (
         spobj_orbital_period - passage_duration
     ) / 2 * np.timedelta64(int(1e6), "us")
     expected_passage_end_time = expected_passage_start_time + passage_duration * np.timedelta64(
         int(1e6), "us"
     )
-    end_time = to_datetime64_us(start_time) + spobj_orbital_period * np.timedelta64(
+    end_time = start_time_dt64 + spobj_orbital_period * np.timedelta64(
         int(1e6), "us"
     )  # simulation end time
 
@@ -106,92 +112,109 @@ def test_south_to_north_circular_orbit():
         degrees=True,
     )
 
-    prop_interp = InterpolatedPropagation.from_space_object(
-        space_object=spobj,
-        propagator=propagator.Kepler(propagator.KeplerSettings()),
-        interpolator_class=interpolation.Legendre8,
-        start_time=start_time,
-        end_time=Time(end_time),
-        time_step=dsec_sampling_intv,
+    spobj_delta_secs: npt.NDArray[types.Float64_as_sec] = np.linspace(
+        0, spobj.state.period[0], num_prop_steps + 1
     )
 
-    tx_0_stn = radar.Station(
-        lat=0.0,
-        lon=0.0,
-        alt=0.0,
-        min_elevation=0.0,
-        beam=pyant.models.Isotropic(),
-        frequency=233e6,  # same as eisat_3d
-        beam_parameters=pyant.models.IsotropicParams(),
-        uid=0,
+    spobj_state: types.EcefStates = propagator.Kepler(propagator.KeplerSettings()).propagate(
+        spobj, spobj_delta_secs
     )
 
-    # NOTE: physically the same as tx_0_stn, but has a different id;
+    # NOTE: all stations are physically the same but just have a different id;
     #   this is done so that we can retain both tx and rx pointings in the schedule,
-    #   as FenceScanController will drop the rx entries if both tx and rx use the same station.
-    rx_0_stn = radar.Station(
-        lat=0.0,
-        lon=0.0,
-        alt=0.0,
-        min_elevation=0.0,
-        beam=pyant.models.Isotropic(),
-        frequency=233e6,  # same as eisat_3d
-        beam_parameters=pyant.models.IsotropicParams(),
-        uid=1,
+    #   as `pointing.fence_scanning` will drop the rx entries if both tx and rx use the same station.
+    tx_stn, *rx_stns = [
+        radar.Station(
+            lat=0.0,
+            lon=0.0,
+            alt=0.0,
+            min_elevation=0.0,
+            beam=pyant.models.Isotropic(),
+            frequency=233e6,  # same as eisat_3d
+            beam_parameters=pyant.models.IsotropicParams(),
+            uid=idx,
+        )
+        for idx in [0, 1, 2]
+    ]
+
+    passages = passage.find_simultaneous_passages(
+        dt=spobj_delta_secs,
+        space_object=spobj,
+        states=spobj_state[:3, ...],
+        tx_station=tx_stn,
+        rx_stations=rx_stns,
+        epoch=start_time_dt64,
     )
 
-    # An offseted station for testing behaviours related to `min_elevation`
-    rx_1_stn = radar.Station(
-        lat=1e-2,
-        lon=0.0,
-        alt=0.0,
-        min_elevation=5.0,
-        beam=pyant.models.Isotropic(),
-        frequency=233e6,  # same as eisat_3d
-        beam_parameters=pyant.models.IsotropicParams(),
-        uid=2,
+    exp_detail = types.ExperimentDetail(
+        id=0,
+        coh_int_bandwidth=1.0,
+        ipp=1.0,
+        pulse_length=1.0,
+        power=5000000.0,
+        bandwidth=52.08333333333333,
+        duty_cycle=1.0,
+        noise_temp=150.0,
+        slice_duration=np.timedelta64(10_000, "us"),  # 10ms
     )
 
-    fence_scan_ctrl = pointing.fence_scanning(
-        tx_station=tx_0_stn,
-        rx_stations=[rx_0_stn, rx_1_stn],
-        exp_detail=types.ExperimentDetail(
-            id=0,
-            coh_int_bandwidth=1.0,
-            ipp=1.0,
-            pulse_length=1.0,
-            power=5000000.0,
-            bandwidth=52.08333333333333,
-            duty_cycle=1.0,
-            noise_temp=150.0,
-            slice_duration=control_slice_duration,
-        ),
+    fence_sch = pointing.fence_scanning(
+        start_time=start_time_dt64,
+        end_time=end_time,
         azimuth=90,  # sweep from east to west
         min_elevation=0,
         pointings_per_cycle=40,
         scan_range=scan_ranges,
+        tx_station=tx_stn,
+        rx_stations=rx_stns,
+        exp_id=exp_detail.id,
+        slice_duration=exp_detail.slice_duration,
     )
 
-    fence_sch = fence_scan_ctrl.generate(start_time, end_time)
-
-    sim = StxMrxSimulation(
-        schedule=fence_sch,
-        epoch=start_time,
-        start_time=start_time,
-        end_time=end_time,
-        space_objects=[spobj],
-        interpolated_propagations=[prop_interp],
+    spobj_abs_times = spobj_delta_secs * np.timedelta64(1, "s") + start_time_dt64
+    spobj_abs_time_indexer: pd.Series[int] = pd.Series(
+        np.arange(len(spobj_abs_times)), index=spobj_abs_times
     )
+    result_states: list[tx_rx_pair_state.TxRxPairState] = []
+    for rx_stn in rx_stns:
+        txrx_state = tx_rx_pair_state.gather_from_passages_schedule_dataframe(
+            passages=passages,
+            sch=fence_sch,
+        )[(tx_stn.uid, rx_stn.uid)]
 
-    sim_result = stx_mrx_simulation.run(
-        sim_units_param=sim.prepare_simulation_unit_params(), show_progress_bar=True
-    )
+        # TODO: this is slightly cleaner but does not work with multiindex
+        # intersection = txrx_state.index.get_level_values("time").intersection(
+        #     spobj_abs_time_indexer.index
+        # )
 
-    # assert the number of `Passage`, `SimulationUnit` and `Observation` are expected
-    assert sum(len(sim_unit_list) for sim_unit_list in sim_units_dict.values()) == 2 # 2 `SimulationUnit` in total; fmt: skip
-    assert len(sim_units_dict[0][0].passages) == 1  # each unit has 1 `Passage`
-    assert len(sim_units_dict[0][1].passages) == 1  # each unit has 1 `Passage`
-    assert sum(len(obss_list) for obss_list in obss_dict.values()) == 3 # 3 `Observaition` in total; fmt: skip
+        # reindexed_txrx_state = txrx_state.reindex(intersection)
+        # reindexed_spobj_state = spobj_state[:, spobj_abs_time_indexer.reindex(intersection)]
+
+        txrx_state_time_idx = txrx_state.index.get_level_values("time")
+        txrx_state_mask = txrx_state_time_idx.isin(spobj_abs_time_indexer.index)
+
+        reindexed_txrx_state = txrx_state[txrx_state_mask]
+        reindexed_spobj_state = spobj_state[
+            :, spobj_abs_time_indexer.reindex(txrx_state_time_idx[txrx_state_mask])
+        ]
+
+        result_state = tx_rx_pair_state.simulate(
+            txrx_state=reindexed_txrx_state,
+            spobj_state=reindexed_spobj_state,
+            spobj_diameter=spobj.d,
+            spobj_radar_albedo=spobj.properties.get("radar_albedo", 1.0),
+            tx_station=tx_stn,
+            rx_station=rx_stn,
+            exp_detail_map={exp_detail.id: exp_detail},
+        )
+        result_states.append(result_state)
+
+    # assert the number and info of passages are expected
+    assert len(passages) == 1  # 1 `Passage` in total
+    assert passages[0].tx_station.uid == tx_stn.uid  # with tx_stn in tx_station
+    assert (all(
+        rx_station.uid == rx_stn.uid for rx_station, rx_stn in zip(passages[0].rx_stations, rx_stns)
+    ))  # with rx_stns in rx_stations; fmt: skip
 
     for obs in obss_dict[0]:
         rx_schedule_slice = obs.index_into_schedule_dataframe(fence_sch).rx
