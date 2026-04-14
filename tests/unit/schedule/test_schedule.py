3ed45b0eb645a088a7a91e3688007cbc7e74e01c
tszhinh <tsz-hin.heung@irf.se>
2026-04-14 09:01:45 +0200

    merged files in 'schedule' into a single module

--- tests/unit/schedule/test_schedule.py
+++ tests/unit/schedule/test_schedule.py
@@ -1,8 +1,6 @@
-import sqlite3, typing as t
 import numpy as np
 import pandas as pd
 from sorts import schedule
-from sorts.schedule import schedule_dataframe, ScheduleKey
 
 
 def setup_function():
@@ -16,26 +14,28 @@ def setup_function():
 
 
 def test_empty_schedule_dataframe():
-    df = schedule_dataframe.empty()
-    schedule_dataframe.validate(df)
+    df = schedule.empty()
+    schedule.validate(df)
 
     return
 
 
 def test_schedule_by_priority():
-    exp_00_sch = schedule_dataframe.from_rows([
+    _K = schedule.ScheduleKey
+
+    exp_00_sch = schedule.from_rows([
         [0,0,0, pd.Timestamp("2026-02-11 00:00:00.123456789", unit="us"), pd.Timestamp("2026-02-11 00:00:59.123456789", unit="us"), 0.1,0.2,0.3],
         [0,0,0, pd.Timestamp("2026-02-11 00:01:00.123456789", unit="us"), pd.Timestamp("2026-02-11 00:01:59.123456789", unit="us"), 0.1,0.2,0.3],
         [0,0,0, pd.Timestamp("2026-02-11 00:02:00.123456789", unit="us"), pd.Timestamp("2026-02-11 00:02:59.123456789", unit="us"), 0.1,0.2,0.3],
         [0,0,0, pd.Timestamp("2027-02-11 00:02:00.123456789", unit="us"), pd.Timestamp("2026-02-11 00:02:59.123456789", unit="us"), 0.1,0.2,0.3],
     ]) # fmt: skip
 
-    exp_01_sch = schedule_dataframe.from_rows([
+    exp_01_sch = schedule.from_rows([
         [1,0,0, pd.Timestamp("2026-02-11 00:01:00.123456789", unit="us"), pd.Timestamp("2026-02-11 00:01:59.123456789", unit="us"), 0.1,0.2,0.3],
         [1,1,0, pd.Timestamp("2026-02-11 00:01:00.123456789", unit="us"), pd.Timestamp("2026-02-11 00:01:59.123456789", unit="us"), 0.1,0.2,0.3],
     ]) # fmt: skip
 
-    exp_02_sch = schedule_dataframe.from_rows([
+    exp_02_sch = schedule.from_rows([
         [2,0,0, pd.Timestamp("2026-02-11 00:01:00.123456789", unit="us"), pd.Timestamp("2026-02-11 00:01:59.123456789", unit="us"), 0.1,0.2,0.3],
         [2,1,0, pd.Timestamp("2026-02-11 00:02:00.123456789", unit="us"), pd.Timestamp("2026-02-11 00:02:59.123456789", unit="us"), 0.1,0.2,0.3],
     ]) # fmt: skip
@@ -43,37 +43,37 @@ def test_schedule_by_priority():
     start_time = "2026-02-11"
     end_time = "2026-03-11"
 
-    df = schedule_dataframe.schedule_by_priority([exp_00_sch, exp_01_sch, exp_02_sch], [0, 1, 0])
+    df = schedule.schedule_by_priority([exp_00_sch, exp_01_sch, exp_02_sch], [0, 1, 0])
 
     assert (
-        1 not in df[ScheduleKey.exp_num].values
+        1 not in df[_K.exp_num].values
     ), "Entries with the same `exp_num` and `start_time` should be removed together, not matter the stations."
 
     assert (
-        2 in df[ScheduleKey.exp_num].values
+        2 in df[_K.exp_num].values
     ), "Entries with the same `exp_num` but different `start_time` should not be removed together."
 
     assert (
-        all(df[ScheduleKey.start_time] >= start_time)
-        and all(df[ScheduleKey.end_time] < end_time)
+        all(df[_K.start_time] >= start_time)
+        and all(df[_K.end_time] < end_time)
     ), "Entries outside of specified time range should not be included." # fmt: skip
 
     return
 
 
 def test_schedule_by_priority_by_very_simple_case():
-    sch_0 = schedule_dataframe.from_rows(
+    sch_0 = schedule.from_rows(
         [
             (0, 0, 0, "2026-02-11 00:00:00", "2026-02-11 00:00:59", 0.1, 0.2, 0.3),
             (0, 0, 0, "2026-02-11 00:02:00", "2026-02-11 00:02:59", 0.1, 0.2, 0.3),
         ]
     )
-    sch_1 = schedule_dataframe.from_rows(
+    sch_1 = schedule.from_rows(
         [
             (0, 0, 0, "2026-02-11 00:00:30", "2026-02-11 00:00:59", 0.1, 0.2, 0.3),
         ]
     )
-    result = schedule_dataframe.schedule_by_priority(schs=[sch_0, sch_1], priorities=[0, 1])
+    result = schedule.schedule_by_priority(schs=[sch_0, sch_1], priorities=[0, 1])
 
     assert pd.DataFrame.equals(result, sch_0)
     return
@@ -94,7 +94,7 @@ def test_time_overlapped_mask():
         dtype="datetime64[us]",
     )
 
-    mask = schedule_dataframe.time_overlapped_mask(start_time=start_time, end_time=end_time)
+    mask = schedule.time_overlapped_mask(start_time=start_time, end_time=end_time)
 
     assert np.all(
         mask == [
@@ -109,7 +109,7 @@ def test_time_overlapped_mask():
 def test_priority_loser_mask():
     priorities = np.array([0, 1, 0], dtype=np.int64)
 
-    mask = schedule_dataframe.priority_loser_mask(priority=priorities)
+    mask = schedule.priority_loser_mask(priority=priorities)
 
     assert np.all(
         mask == [
@@ -124,10 +124,10 @@ def test_priority_loser_mask():
 def test_get_tx_rx_pointing_pairs_uses_inner_join():
     """`get_tx_rx_pointing_pairs` should use inner join over on columns `exp_num` `start_time` `end_time`."""
 
-    _SK = schedule_dataframe.ScheduleKey
+    _SK = schedule.ScheduleKey
     _PK = schedule.TxRxPointingPairsKey
 
-    df_input = schedule_dataframe.from_rows(
+    df_input = schedule.from_rows(
         [
             [0,0,0, pd.Timestamp("2026-02-11 01:00:00", unit="us"), pd.Timestamp("2026-02-11 02:00:00", unit="us"), 0,0,0], # fmt: skip
             [0,0,0, pd.Timestamp("2026-02-11 02:00:00", unit="us"), pd.Timestamp("2026-02-11 03:00:00", unit="us"), 0,0,0], # fmt: skip
@@ -136,7 +136,7 @@ def test_get_tx_rx_pointing_pairs_uses_inner_join():
         ]
     )
 
-    df_result = schedule_dataframe.get_tx_rx_pointing_pairs(
+    df_result = schedule.get_tx_rx_pointing_pairs(
         sch=df_input,
         start_time=df_input[_SK.start_time].min(),
         end_time=df_input[_SK.end_time].max(),
